7bb2af0d312f757bfe34cdbd7ca5bcb24229e9bf
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-06-25 21:00:06 +0900

    프레임 구조 정리중

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from "next/app";
+import LayoutContext from "@/components/layout";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutContext>
+      <Component {...pageProps} />
+    </LayoutContext>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>df</div>;
+  return <div>index</div>;
 }

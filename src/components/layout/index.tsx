1d5732f05313ac0a981f8badb689d7fe901efd3c
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-06-28 22:33:17 +0900

    테일 윈드 적용

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,19 @@
 import Box from "@mui/material/Box";
 import { ReactNode } from "react";
-
+import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
 interface ILayoutProps {
   children: ReactNode;
 }
 
 const LayoutContext = ({ children }: ILayoutProps) => {
   return (
-    <Box sx={{ display: "flex" }}>
-      <Box className="gray-900 w-3">layout</Box>
+    <Box className="flex">
+      <Box className="bg-gray-900 w-12 flex">
+        <Box className="text-gray-100">
+          <InsertDriveFileOutlinedIcon />
+          <InsertDriveFileOutlinedIcon className="ml-[-20px] mt-[-5px]" />
+        </Box>
+      </Box>
       <Box className="flex-1">{children}</Box>
     </Box>
   );

926ec4d99be922dc0101241c9090e740fd7d6b1b
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-06-29 21:21:25 +0900

    상태 추가

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,17 +1,25 @@
 import Box from "@mui/material/Box";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 interface ILayoutProps {
   children: ReactNode;
 }
 
+type TMode = "file" | "search";
+
 const LayoutContext = ({ children }: ILayoutProps) => {
+  const [mode, setMode] = useState<TMode>("file");
   return (
     <Box className="flex">
-      <Box className="bg-gray-900 w-12 flex">
-        <Box className="text-gray-100">
+      <Box className="bg-gray-900 w-12 flex text-gray-100 flex-col items-center gap-2 py-2 h-screen">
+        <Box>
           <InsertDriveFileOutlinedIcon />
           <InsertDriveFileOutlinedIcon className="ml-[-20px] mt-[-5px]" />
+        </Box>
+        <Box>
+          <SearchOutlinedIcon />
         </Box>
       </Box>
       <Box className="flex-1">{children}</Box>

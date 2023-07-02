b6d18d5acb1bd1ca68af490d600b95be18cb2271
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-07-02 22:24:21 +0900

    탐색기 샘플 데이터 작성

--- src/components/Explorer/index.tsx
+++ src/components/Explorer/index.tsx
@@ -4,27 +4,63 @@ import TreeItem from "@mui/lab/TreeItem";
 import TreeView from "@mui/lab/TreeView";
 import Box from "@mui/material/Box";
 
+interface ITree {
+  id: string;
+  label: string;
+  path?: string;
+  children?: ITree[];
+}
+
+const sample: ITree[] = [
+  {
+    id: "profile",
+    label: "profile",
+    children: [{ id: "profile-index", label: "index.html" }],
+  },
+  {
+    id: "project",
+    label: "project",
+    children: [
+      { id: "next-blog", label: "next-blog", path: "/profile/next-blog" },
+      { id: "next-blog-2", label: "next-blog", path: "/profile/next-blog" },
+      {
+        id: "next-blog-3",
+        label: "next-blog",
+        path: "/profile/next-blog",
+        children: [
+          { id: "next-blog-4", label: "next-blog", path: "/profile/next-blog" },
+          { id: "next-blog-5", label: "next-blog", path: "/profile/next-blog" },
+        ],
+      },
+    ],
+  },
+];
+
 const Explorer = () => {
   return (
-    <Box>
+    <Box className="bg-gray-900  text-gray-100 h-screen min-w-[250px] p-2">
       <TreeView
         aria-label="file system navigator"
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
         sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
       >
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-        </TreeItem>
-        <TreeItem nodeId="5" label="Documents">
-          <TreeItem nodeId="10" label="OSS" />
-          <TreeItem nodeId="6" label="MUI">
-            <TreeItem nodeId="8" label="index.js" />
-          </TreeItem>
-        </TreeItem>
+        {sample.map((item) => {
+          return <Explorer.item key={`tree_${item.id}`} {...item} />;
+        })}
       </TreeView>
     </Box>
   );
 };
 
 export default Explorer;
+
+Explorer.item = ({ id, label, path, children }: ITree) => {
+  return (
+    <TreeItem nodeId={id} label={label}>
+      {children?.map((item) => {
+        return <Explorer.item key={`tree_${item.id}`} {...item} />;
+      })}
+    </TreeItem>
+  );
+};

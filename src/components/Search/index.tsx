7a9cfcc1b0608bb9d123d5fba2eff9933e5bc324
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-07-06 22:34:10 +0900

    입력 창 디자인

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,27 @@
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Box from "@mui/material/Box";
+import { useState } from "react";
 
 const Search = () => {
   const data = {};
+  const [keyword, setKeyword] = useState("");
+  const handleChangeKeyword: React.KeyboardEventHandler<HTMLInputElement> = ({
+    currentTarget: { value },
+  }) => {
+    setKeyword(value);
+  };
   return (
     <Box className="bg-gray-900  text-gray-100 h-screen min-w-[250px] p-2">
-      <input type="text" placeholder="Search" className="w-100" />
+      <Box className="flex items-center">
+        <ArrowForwardIosIcon className="text-sm mr-1" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full text-xs py-1 px-2 bg-gray-700 border-gray-300 border-[0.5px] rounded-sm"
+          value={keyword}
+          onKeyDown={handleChangeKeyword}
+        />
+      </Box>
     </Box>
   );
 };

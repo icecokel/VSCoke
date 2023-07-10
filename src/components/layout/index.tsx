58d3ba43cdfdf741c8356c2b0826eebd8b7726ac
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-07-11 07:11:04 +0900

    헤드 수정, 레이 아웃 수정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,6 +3,8 @@ import Search from "../Search";
 import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Head from "next/head";
 import { ReactNode, useState } from "react";
 
 interface ILayoutProps {
@@ -31,41 +33,50 @@ type TTab = "file" | "search";
 const Layout = ({ children }: ILayoutProps) => {
   const [tab, setTab] = useState<TTab>("file");
 
-  const handleChangeTab: React.MouseEventHandler<HTMLDivElement> = ({ currentTarget: { id } }) => {
+  const handleChangeTab: React.MouseEventHandler<HTMLDivElement> = ({
+    currentTarget: { id },
+  }) => {
     setTab(id as TTab);
   };
   return (
-    <Box className="flex">
-      <Box className="flex h-screen w-12 flex-col items-center gap-2 bg-gray-900 py-2 text-gray-100 border-r-2 border-r-gray-500">
-        {TABS.map(({ name, icon }) => {
-          return (
-            <>
-              {tab === name ? (
-                <Box
-                  key={`tab_${name}`}
-                  id={name}
-                  onClick={handleChangeTab}
-                  className="flex h-10 items-center justify-center border-l-2 cursor-pointer w-full border-l-blue-100 bg-gray-900"
-                >
-                  {icon}
-                </Box>
-              ) : (
-                <Box
-                  key={`tab_${name}`}
-                  id={name}
-                  onClick={handleChangeTab}
-                  className="flex h-10 items-center justify-center border-l-2 cursor-pointer w-full border-l-gray-900 bg-gray-900"
-                >
-                  {icon}
-                </Box>
-              )}
-            </>
-          );
-        })}
+    <>
+      <Head>
+        <title>VSCODE</title>
+      </Head>
+      <Box className="flex bg-gray-900">
+        <Box className="flex min-h-screen w-12 flex-col items-center gap-2 border-r-2 border-r-gray-500 bg-gray-900 py-2 text-gray-100">
+          {TABS.map(({ name, icon }) => {
+            return (
+              <>
+                {tab === name ? (
+                  <Box
+                    key={`tab_${name}`}
+                    id={name}
+                    onClick={handleChangeTab}
+                    className="flex h-10 w-full cursor-pointer items-center justify-center border-l-2 border-l-blue-100 bg-gray-900"
+                  >
+                    {icon}
+                  </Box>
+                ) : (
+                  <Box
+                    key={`tab_${name}`}
+                    id={name}
+                    onClick={handleChangeTab}
+                    className="flex h-10 w-full cursor-pointer items-center justify-center border-l-2 border-l-gray-900 bg-gray-900"
+                  >
+                    {icon}
+                  </Box>
+                )}
+              </>
+            );
+          })}
+        </Box>
+        {tab === "file" ? <Explorer /> : <Search />}
+        <Container className="min-h-screen flex-1 p-20 text-white md:p-10">
+          {children}
+        </Container>
       </Box>
-      {tab === "file" ? <Explorer /> : <Search />}
-      <Box className="flex-1">{children}</Box>
-    </Box>
+    </>
   );
 };
 

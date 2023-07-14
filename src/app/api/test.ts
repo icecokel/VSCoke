f2f841c38e60dc888fddfe74e69274af58a5d149
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-07-14 22:02:00 +0900

    라우팅 변경  page -> app

--- src/app/api/test.ts
+++ src/app/api/test.ts
@@ -11,20 +11,5 @@ export default async function handler(
   const databaseId = "4e781252909b4ab694f2988282aba05f";
   const response = await notion.databases.retrieve({ database_id: databaseId });
 
-  const response2 = await notion.databases.query({
-    database_id: databaseId,
-    // filter: {
-    //   and: [
-    //     {
-
-    //     }
-    //   ]
-
-    // }
-  });
-  response2.results.forEach((row: any) => {
-    console.log(row.properties["상품 명"].title[0].plain_text);
-  });
-
   res.status(200).json({ name: "John Doe" });
 }

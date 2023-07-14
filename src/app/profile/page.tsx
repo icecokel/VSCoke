3c6777a3cbdadf9720ac5225cc270d6ff915708d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-07-14 22:02:00 +0900

    라우팅 변경  page -> app

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,12 @@
-import { NextPage } from "next";
 import Profile from "@/components/Profile";
+import { NextPage } from "next";
 
 /**
  * 이력서 페이지
  * @returns 이력서 컴포넌트
  */
-const ProfilePage :NextPage = () => {
-  return <Profile/>
+const ProfilePage: NextPage = () => {
+  return <Profile />;
 };
 
 export default ProfilePage;
-

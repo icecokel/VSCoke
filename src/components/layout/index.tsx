81c729ac947523367264d1b2bb8582e622745c98
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-07-16 00:57:43 +0900

    타이틀 수정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -41,44 +41,39 @@ const Layout = ({ children }: ILayoutProps) => {
     setTab(id as TTab);
   };
   return (
-    <>
-      <Head>
-        <title>VSCODE</title>
-      </Head>
-      <Box className="flex bg-gray-900">
-        <Box className="flex min-h-screen w-12 flex-col items-center gap-2 border-r-2 border-r-gray-500 bg-gray-900 py-2 text-gray-100">
-          {TABS.map(({ name, icon }) => {
-            return (
-              <>
-                {tab === name ? (
-                  <Box
-                    key={`tab_${name}`}
-                    id={name}
-                    onClick={handleChangeTab}
-                    className="flex h-10 w-full cursor-pointer items-center justify-center border-l-2 border-l-blue-100 bg-gray-900"
-                  >
-                    {icon}
-                  </Box>
-                ) : (
-                  <Box
-                    key={`tab_${name}`}
-                    id={name}
-                    onClick={handleChangeTab}
-                    className="flex h-10 w-full cursor-pointer items-center justify-center border-l-2 border-l-gray-900 bg-gray-900"
-                  >
-                    {icon}
-                  </Box>
-                )}
-              </>
-            );
-          })}
-        </Box>
-        {tab === "file" ? <Explorer /> : <Search />}
-        <Container className="min-h-screen flex-1 p-20 text-white md:p-10">
-          {children}
-        </Container>
+    <Box className="flex bg-gray-900">
+      <Box className="flex min-h-screen w-12 flex-col items-center gap-2 border-r-2 border-r-gray-500 bg-gray-900 py-2 text-gray-100">
+        {TABS.map(({ name, icon }) => {
+          return (
+            <>
+              {tab === name ? (
+                <Box
+                  key={`tab_${name}`}
+                  id={name}
+                  onClick={handleChangeTab}
+                  className="flex h-10 w-full cursor-pointer items-center justify-center border-l-2 border-l-blue-100 bg-gray-900"
+                >
+                  {icon}
+                </Box>
+              ) : (
+                <Box
+                  key={`tab_${name}`}
+                  id={name}
+                  onClick={handleChangeTab}
+                  className="flex h-10 w-full cursor-pointer items-center justify-center border-l-2 border-l-gray-900 bg-gray-900"
+                >
+                  {icon}
+                </Box>
+              )}
+            </>
+          );
+        })}
       </Box>
-    </>
+      {tab === "file" ? <Explorer /> : <Search />}
+      <Container className="min-h-screen flex-1 p-20 text-white md:p-10">
+        {children}
+      </Container>
+    </Box>
   );
 };
 

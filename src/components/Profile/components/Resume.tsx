21ff2cf5a96c262de1c45aa80e835afbbd47f3c3
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-17 22:44:30 +0900

    데이터 구조 적립

--- src/components/Profile/components/Resume.tsx
+++ src/components/Profile/components/Resume.tsx
@@ -1,5 +1,46 @@
+"use client";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
 const Resume = () => {
-  return <div></div>;
+  const [page, setPage] = useState(1);
+  const data: any = {};
+
+  return <Box></Box>;
 };
 
 export default Resume;
+
+interface IPageProps {
+  corporate: string;
+  team: string;
+  items: IPageItem[];
+}
+
+interface IPageItem {
+  title: string;
+  periodStart: string;
+  PeriodEnd: string;
+  jobs: string;
+  skiils: string[];
+}
+
+Resume.page = ({ corporate, team, items }: IPageProps) => {
+  return (
+    <Box>
+      <Typography>{corporate}</Typography>
+      <Typography>{team}</Typography>
+      <ul>
+        {items.map(({ title, periodStart, PeriodEnd, jobs, skiils }) => {
+          return (
+            <li key={title}>
+              <Typography>{team}</Typography>
+            </li>
+          );
+        })}
+      </ul>
+    </Box>
+  );
+};

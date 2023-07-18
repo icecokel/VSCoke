d3e761b1290beb0aa2fab24556afb6147c3ba5cf
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-18 21:09:23 +0900

    임시 화면 추가

--- src/components/Profile/components/Resume.tsx
+++ src/components/Profile/components/Resume.tsx
@@ -6,17 +6,25 @@ import { useState } from "react";
 
 const Resume = () => {
   const [page, setPage] = useState(1);
-  const data: any = {};
+  const data: IPageProps[] = sampleData;
 
-  return <Box></Box>;
+  const info = data.find((row) => row.page === page);
+
+  if (!info) return <></>;
+  return (
+    <Box>
+      <Resume.page {...info} />
+    </Box>
+  );
 };
 
 export default Resume;
 
 interface IPageProps {
+  page: number;
   corporate: string;
   team: string;
-  items: IPageItem[];
+  items?: IPageItem[];
 }
 
 interface IPageItem {
@@ -33,14 +41,55 @@ Resume.page = ({ corporate, team, items }: IPageProps) => {
       <Typography>{corporate}</Typography>
       <Typography>{team}</Typography>
       <ul>
-        {items.map(({ title, periodStart, PeriodEnd, jobs, skiils }) => {
-          return (
-            <li key={title}>
-              <Typography>{team}</Typography>
-            </li>
-          );
-        })}
+        {items &&
+          items.map(({ title, periodStart, PeriodEnd, jobs, skiils }) => {
+            return (
+              <li key={title}>
+                <Typography>{team}</Typography>
+              </li>
+            );
+          })}
       </ul>
     </Box>
   );
 };
+
+const sampleData: IPageProps[] = [
+  {
+    page: 1,
+    corporate: "코드 크레용",
+    team: "개발팀",
+  },
+  {
+    page: 2,
+    corporate: "Allofthem",
+    team: "개발팀",
+    items: [
+      {
+        title: "상품 보험 가입 관리 사이트 제작",
+        periodStart: "2023.03",
+        PeriodEnd: "2023.04",
+        jobs: `1. 기획, 설계 검토
+
+        2. 보고서 페이지 작성
+         - 일/월/년 별 가입한 사용자 통계 차트 화면 개발 
+         - 차트 정보 API 개발
+         - 파일 내보내기 기능 개발
+         
+        3. 관리자 가입 페이지 작성
+        `,
+        skiils: [
+          "Next",
+          "React",
+          "TypeScript",
+          "Redux",
+          "AppSync(GraphQL)",
+          "DynamoDB",
+          "gitLab",
+          "CI/CD",
+          "sass",
+        ],
+      },
+    ],
+  },
+];

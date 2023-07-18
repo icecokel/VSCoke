1e515447debf1afaf7ec0c563f2b56c1d92f692a
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-18 21:09:01 +0900

    경력서 페이지 추가

---
+++ src/app/profile/resume/page.tsx
@@ -0,0 +1,12 @@
+import Resume from "@/components/Profile/components/Resume";
+import { NextPage } from "next";
+
+/**
+ * 경력 페이지
+ * @returns 경력 컴포넌트
+ */
+const ProfilePage: NextPage = () => {
+  return <Resume />;
+};
+
+export default ProfilePage;

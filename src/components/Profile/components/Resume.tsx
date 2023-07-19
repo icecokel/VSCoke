88c1d6ac0a59c9f3005202b4b264c61302716555
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-19 20:56:27 +0900

    경력 컴포넌트 연결 현재 상태 표시

--- src/components/Profile/components/Resume.tsx
+++ src/components/Profile/components/Resume.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import MobileStepper from "@mui/material/MobileStepper";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 
@@ -10,9 +16,41 @@ const Resume = () => {
 
   const info = data.find((row) => row.page === page);
 
+  const handleClickArrow = (type: "back" | "forward") => () => {
+    setPage((prev) => (type === "back" ? prev - 1 : prev + 1));
+  };
+
   if (!info) return <></>;
   return (
     <Box>
+      <MobileStepper
+        variant="progress"
+        steps={data.length + 1}
+        position="static"
+        activeStep={page}
+        className="mb-5 bg-gray-900"
+        sx={{ flexGrow: 1 }}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleClickArrow("forward")}
+            disabled={page === data.length}
+          >
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button
+            size="small"
+            onClick={handleClickArrow("back")}
+            disabled={page === 1}
+          >
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
       <Resume.page {...info} />
     </Box>
   );
@@ -38,14 +76,52 @@ interface IPageItem {
 Resume.page = ({ corporate, team, items }: IPageProps) => {
   return (
     <Box>
-      <Typography>{corporate}</Typography>
-      <Typography>{team}</Typography>
+      <Box className="flex items-end gap-2">
+        <Typography variant="h5" fontWeight={700}>
+          {corporate}
+        </Typography>
+        <Typography
+          variant="h6"
+          fontSize={16}
+          fontWeight={700}
+          className="text-gray-300"
+        >
+          {team}
+        </Typography>
+      </Box>
+      <Divider className="my-5 border-gray-50" />
       <ul>
         {items &&
           items.map(({ title, periodStart, PeriodEnd, jobs, skiils }) => {
             return (
               <li key={title}>
-                <Typography>{team}</Typography>
+                <Box className="flex items-end gap-2">
+                  <Typography variant="h6" fontWeight={700}>
+                    {title}
+                  </Typography>
+                  <Typography className="text-gray-300" variant="body2">
+                    {periodStart} ~ {PeriodEnd}
+                  </Typography>
+                </Box>
+                <Typography variant="body1" fontWeight={600} className="ml-3">
+                  <pre>
+                    <code>{jobs}</code>
+                  </pre>
+                </Typography>
+                <Typography variant="body2" className="mb-4">
+                  사용된 기술
+                </Typography>
+                <Box>
+                  {skiils.map((item, index) => (
+                    <Chip
+                      key={`skill_${index}`}
+                      label={item}
+                      size="small"
+                      variant="outlined"
+                      className="mb-2 mr-2 select-none p-1 text-white "
+                    />
+                  ))}
+                </Box>
               </li>
             );
           })}
@@ -69,14 +145,48 @@ const sampleData: IPageProps[] = [
         title: "상품 보험 가입 관리 사이트 제작",
         periodStart: "2023.03",
         PeriodEnd: "2023.04",
-        jobs: `1. 기획, 설계 검토
+        jobs: `
+1. 기획, 설계 검토
 
-        2. 보고서 페이지 작성
-         - 일/월/년 별 가입한 사용자 통계 차트 화면 개발 
-         - 차트 정보 API 개발
-         - 파일 내보내기 기능 개발
-         
-        3. 관리자 가입 페이지 작성
+2. 보고서 페이지 작성
+  - 일/월/년 별 가입한 사용자 통계 차트 화면 개발 
+  - 차트 정보 API 개발
+  - 파일 내보내기 기능 개발
+  
+3. 관리자 가입 페이지 작성
+        `,
+        skiils: [
+          "Next",
+          "React",
+          "TypeScript",
+          "Redux",
+          "AppSync(GraphQL)",
+          "DynamoDB",
+          "gitLab",
+          "CI/CD",
+          "sass",
+        ],
+      },
+    ],
+  },
+  {
+    page: 3,
+    corporate: "Allofthem",
+    team: "개발팀",
+    items: [
+      {
+        title: "상품 보험 가입 관리 사이트 제작",
+        periodStart: "2023.03",
+        PeriodEnd: "2023.04",
+        jobs: `
+1. 기획, 설계 검토
+
+2. 보고서 페이지 작성
+  - 일/월/년 별 가입한 사용자 통계 차트 화면 개발 
+  - 차트 정보 API 개발
+  - 파일 내보내기 기능 개발
+  
+3. 관리자 가입 페이지 작성
         `,
         skiils: [
           "Next",

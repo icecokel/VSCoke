d653ac6e3446c42ad38042d20bf25afbce55ffb3
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-21 20:33:09 +0900

    페이지 전환 방식 수정

--- src/components/Profile/components/Resume.tsx
+++ src/components/Profile/components/Resume.tsx
@@ -1,57 +1,81 @@
 "use client";
 
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
-import Divider from "@mui/material/Divider";
-import MobileStepper from "@mui/material/MobileStepper";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
+import useSWR from "swr";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
 
 const Resume = () => {
-  const [page, setPage] = useState(1);
+  const [tabValue, setPage] = useState(1);
+
+  const {} = useSWR("/api/getPage?id=a2e9e58d2f6c4b23ac52d45e7cb17af1");
   const data: IPageProps[] = sampleData;
 
-  const info = data.find((row) => row.page === page);
+  const info = data.find((row) => row.page === tabValue);
 
-  const handleClickArrow = (type: "back" | "forward") => () => {
-    setPage((prev) => (type === "back" ? prev - 1 : prev + 1));
+  const handleChangeTabs = (event: React.SyntheticEvent, newValue: number) => {
+    setPage(newValue);
   };
 
   if (!info) return <></>;
   return (
     <Box>
-      <MobileStepper
-        variant="progress"
-        steps={data.length + 1}
-        position="static"
-        activeStep={page}
-        className="mb-5 bg-gray-900"
-        sx={{ flexGrow: 1 }}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleClickArrow("forward")}
-            disabled={page === data.length}
-          >
-            Next
-            <KeyboardArrowRight />
-          </Button>
-        }
-        backButton={
-          <Button
-            size="small"
-            onClick={handleClickArrow("back")}
-            disabled={page === 1}
-          >
-            <KeyboardArrowLeft />
-            Back
-          </Button>
-        }
-      />
-      <Resume.page {...info} />
+      <Tabs
+        value={tabValue}
+        onChange={handleChangeTabs}
+        textColor="inherit"
+        variant="fullWidth"
+      >
+        {data.map((item) => {
+          return (
+            <Tab
+              label={
+                <Box className="flex items-end gap-1">
+                  <Typography variant="h6" fontWeight={700} fontSize={18}>
+                    {item.corporate}
+                  </Typography>
+                  <Typography variant="body2" className="text-gray-300">
+                    {item.team}
+                  </Typography>
+                </Box>
+              }
+              value={item.page}
+            />
+          );
+        })}
+      </Tabs>
+      {data.map((item) => {
+        return (
+          <TabPanel value={item.page} index={tabValue}>
+            <Resume.page {...item} />
+          </TabPanel>
+        );
+      })}
     </Box>
   );
 };
@@ -73,60 +97,44 @@ interface IPageItem {
   skiils: string[];
 }
 
-Resume.page = ({ corporate, team, items }: IPageProps) => {
+Resume.page = ({ items }: IPageProps) => {
   return (
-    <Box>
-      <Box className="flex items-end gap-2">
-        <Typography variant="h5" fontWeight={700}>
-          {corporate}
-        </Typography>
-        <Typography
-          variant="h6"
-          fontSize={16}
-          fontWeight={700}
-          className="text-gray-300"
-        >
-          {team}
-        </Typography>
-      </Box>
-      <Divider className="my-5 border-gray-50" />
-      <ul>
-        {items &&
-          items.map(({ title, periodStart, PeriodEnd, jobs, skiils }) => {
-            return (
-              <li key={title}>
-                <Box className="flex items-end gap-2">
-                  <Typography variant="h6" fontWeight={700}>
-                    {title}
-                  </Typography>
-                  <Typography className="text-gray-300" variant="body2">
-                    {periodStart} ~ {PeriodEnd}
-                  </Typography>
-                </Box>
-                <Typography variant="body1" fontWeight={600} className="ml-3">
-                  <pre>
-                    <code>{jobs}</code>
-                  </pre>
+    <ul>
+      {items &&
+        items.map(({ title, periodStart, PeriodEnd, jobs, skiils }) => {
+          return (
+            <li key={title}>
+              <Box className="flex items-end gap-2">
+                <Typography variant="h6" fontWeight={700}>
+                  {title}
                 </Typography>
-                <Typography variant="body2" className="mb-4">
-                  사용된 기술
+                <Typography className="text-gray-300" variant="body2">
+                  {periodStart} ~ {PeriodEnd}
                 </Typography>
-                <Box>
-                  {skiils.map((item, index) => (
-                    <Chip
-                      key={`skill_${index}`}
-                      label={item}
-                      size="small"
-                      variant="outlined"
-                      className="mb-2 mr-2 select-none p-1 text-white "
-                    />
-                  ))}
-                </Box>
-              </li>
-            );
-          })}
-      </ul>
-    </Box>
+              </Box>
+              <Typography variant="body1" fontWeight={600} className="ml-3">
+                <pre>
+                  <code>{jobs}</code>
+                </pre>
+              </Typography>
+              <Typography variant="body2" className="mb-4">
+                사용된 기술
+              </Typography>
+              <Box>
+                {skiils.map((item, index) => (
+                  <Chip
+                    key={`skill_${index}`}
+                    label={item}
+                    size="small"
+                    variant="outlined"
+                    className="mb-2 mr-2 select-none p-1 text-white "
+                  />
+                ))}
+              </Box>
+            </li>
+          );
+        })}
+    </ul>
   );
 };
 
@@ -171,8 +179,41 @@ const sampleData: IPageProps[] = [
   },
   {
     page: 3,
-    corporate: "Allofthem",
+    corporate: "데이터로직스",
     team: "개발팀",
+    items: [
+      {
+        title: "상품 보험 가입 관리 사이트 제작",
+        periodStart: "2023.03",
+        PeriodEnd: "2023.04",
+        jobs: `
+1. 기획, 설계 검토
+
+2. 보고서 페이지 작성
+  - 일/월/년 별 가입한 사용자 통계 차트 화면 개발 
+  - 차트 정보 API 개발
+  - 파일 내보내기 기능 개발
+  
+3. 관리자 가입 페이지 작성
+        `,
+        skiils: [
+          "Next",
+          "React",
+          "TypeScript",
+          "Redux",
+          "AppSync(GraphQL)",
+          "DynamoDB",
+          "gitLab",
+          "CI/CD",
+          "sass",
+        ],
+      },
+    ],
+  },
+  {
+    page: 4,
+    corporate: "데이터로직스",
+    team: "인프라",
     items: [
       {
         title: "상품 보험 가입 관리 사이트 제작",

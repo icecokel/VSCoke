700411459c7929f29e643fcdc267b750fe114e6e
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-22 10:14:48 +0900

    임시 커밋 정리 필요

--- src/util/notionUtil.ts
+++ src/util/notionUtil.ts
@@ -9,3 +9,13 @@ export const getDatabaseById = async (id: string) => {
   // TODO 데이터 파싱
   return response;
 };
+
+export const getPageById = async (id: string) => {
+  console.log(id);
+  const response = await notion.pages.retrieve({ page_id: id });
+
+  console.log(response);
+  return response;
+};
+
+//https://www.notion.so/a2e9e58d2f6c4b23ac52d45e7cb17af1?pvs=4

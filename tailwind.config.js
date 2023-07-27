fa3ad05e428fe6298a3f8d294de5c3dea36b148c
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-27 21:52:50 +0900

    색상 코드 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { colors } = require("./src/styles/colors");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,30 +8,12 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      gray: {
-        50: "#D7D7D7",
-        100: "#CCCCCC",
-        300: "#8C8C8C",
-        500: "#4C4C4C",
-        700: "#2A2A2A",
-        800: "#1F1F1F",
-        900: "#181818",
-      },
-      blue: {
-        100: "#0078D4",
-      },
-      yellow: {
-        100: "#D68F34",
-        200: "#CCA700",
-      },
-      white: "#FFFFFF",
-      black: "#000000",
-    },
+    colors,
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },

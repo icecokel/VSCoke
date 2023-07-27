b8e69a0de8e255b0367e5477479e53af6587f8aa
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-27 21:53:37 +0900

    설정 및 프로바이더 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import LoaderProvider from "@/contexts/LoaderProvider";
+import MuiConfigProvider from "@/contexts/MuiConfigProvider";
 import { SWRProvider } from "@/contexts/SWRProvider";
 
 export default async function RootLayout({
@@ -11,7 +13,11 @@ export default async function RootLayout({
     <html lang="ko">
       <body>
         <SWRProvider>
-          <Sidebar>{children}</Sidebar>
+          <MuiConfigProvider>
+            <LoaderProvider>
+              <Sidebar>{children}</Sidebar>
+            </LoaderProvider>
+          </MuiConfigProvider>
         </SWRProvider>
       </body>
     </html>

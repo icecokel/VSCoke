b34c8bd6facb4cc94448a72851ed3a8d414c28cc
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-07-30 04:24:50 +0900

    컴포넌트 타입변경

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -27,29 +27,34 @@ const TABS = [
 ];
 
 const Sidebar = ({ children }: ILayoutProps) => {
-  const [tab, setTab] = useState<TSidebar | "none">("explore");
+  const [tab, setTab] = useState<TSidebar | "none">("none");
 
-  const handleChangeTab: React.MouseEventHandler<HTMLDivElement> = ({
-    currentTarget: { id },
+  const handleChangeTab: React.MouseEventHandler<HTMLInputElement> = ({
+    currentTarget: { value },
   }) => {
-    setTab(id === tab ? "none" : (id as TSidebar));
+    setTab(value === tab ? "none" : (value as TSidebar));
   };
   return (
     <Box className="flex bg-gray-900">
-      <Box className="flex min-h-screen w-12 flex-col items-center gap-2 border-r-2 border-r-gray-500 bg-gray-900 py-2 text-gray-100">
+      <Box className="z-50 flex min-h-screen w-12 flex-col items-center gap-2 border-r-2 border-r-gray-500 bg-gray-900 py-2 text-gray-100">
         {TABS.map(({ name, icon }) => {
           return (
-            <Box
+            <label
               key={`tab_${name}`}
-              id={name}
-              onClick={handleChangeTab}
               className={twMerge(
                 "flex h-10 w-full cursor-pointer items-center justify-center border-l-2 bg-gray-900",
                 name === tab ? "border-l-blue-100" : "border-l-gray-900"
               )}
             >
               {icon}
-            </Box>
+              <input
+                type="radio"
+                name="tabs"
+                defaultValue={name}
+                className="hidden"
+                onClick={handleChangeTab}
+              />
+            </label>
           );
         })}
       </Box>

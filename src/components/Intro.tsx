f0ba9c1e88d500fe5cc38227a7a7ea731f1794ec
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-08-01 22:32:44 +0900

    MuiStack으로 변경

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,27 +1,32 @@
 "use client";
 
 import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
 const Intro = () => {
   return (
     <div className="p-8">
-      <div className="flex justify-center">
+      <Stack justifyContent={"center"} alignItems={"center"}>
         <Typography variant="h3" fontWeight={700}>
           Welcome VSCoke
         </Typography>
-      </div>
+      </Stack>
       <Divider className="my-5 border-white" />
-      <div className="flex items-end px-10 pb-2 pt-5">
+      <Stack
+        alignItems={"center"}
+        className="px-10 pb-2 pt-5"
+        flexDirection={"row"}
+      >
         <Typography variant="h6">OS가 윈도우인 경우</Typography>
         <div className="m-2 w-fit rounded-md border p-2">
           <Typography variant="h5">F11</Typography>
         </div>
         <Typography variant="h6">눌러 "전체 화면" 으로 보시면</Typography>
-      </div>
-      <div className="flex items-end px-10 pb-5">
+      </Stack>
+      <Stack alignItems={"center"} className="px-10 pb-5" flexDirection={"row"}>
         <Typography variant="h6"> 더욱 좋은 경험이 될겁니다</Typography>
-      </div>
+      </Stack>
     </div>
   );
 };

8721d1ff6877b678a80f5ea645770035eb29d910
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-08-15 20:26:35 +0900

    좌클릭 이벤트 추가

--- src/components/HistoryTabs/index.tsx
+++ src/components/HistoryTabs/index.tsx
@@ -1,52 +1,57 @@
 "use client";
 
+import { IHistoryItem, historyAtom } from "@/atom/history";
 import { IHaveChildren } from "@/models/common";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import { useEffect, useState } from "react";
+import { useAtom } from "jotai/react";
+import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
-interface IHistoryItem {
-  title: string;
-  path: string;
-  isAactive: boolean;
-}
-
-const sample: IHistoryItem[] = [
-  { title: "ddfdf", path: "dfdf", isAactive: true },
-  { title: "ddfdf", path: "dfdf", isAactive: false },
-];
-
 const HistoryTabs = ({ children }: IHaveChildren) => {
-  const [history, setHistory] = useState<IHistoryItem[]>(sample);
+  const [history, setHistory] = useAtom(historyAtom);
+  const { push } = useRouter();
 
-  useEffect(() => {}, []);
+  const handleClickTab = ({ isAactive, path, title }: IHistoryItem) => {
+    if (!isAactive) {
+      push(path);
+      setHistory(prev => prev.map(item => ({ ...item, isAactive: item.path === path })));
+    }
+  };
+
+  const handleRightClickTab = () => {
+    console.log("우클릭");
+  };
 
   return (
-    <div>
+    <div className="w-full bg-gray-800">
       <Stack direction={"row"} className="bg-gray-900">
         {history.map(({ isAactive, path, title }) => (
-          <div
-            key={`tab_${path}`}
-            className={twMerge(
-              "border border-gray-300/60 border-l-[0px]",
-              isAactive && "bg-gray-800 border-b-[0px]",
-            )}
-          >
-            <Typography
+          <Tooltip key={`tab_${path}`} title={`${path}/${title}`}>
+            <div
               className={twMerge(
-                "text-gray-300/80 py-[6px] px-[20px]",
-                isAactive && "text-yellow-200/95 font-medium border-t border-t-blue-100 pt-[5px]",
+                "border border-gray-300/60 border-l-[0px]",
+                isAactive && "bg-gray-800 border-b-[0px]",
               )}
-              fontSize={14}
+              onClick={() => handleClickTab({ isAactive, path, title })}
+              onContextMenu={handleRightClickTab}
             >
-              {title}
-            </Typography>
-          </div>
+              <Typography
+                className={twMerge(
+                  "text-gray-300/80 py-[6px] px-[20px]",
+                  isAactive && "text-yellow-200/95 font-medium border-t border-t-blue-100 pt-[5px]",
+                )}
+                fontSize={14}
+              >
+                {title}
+              </Typography>
+            </div>
+          </Tooltip>
         ))}
       </Stack>
-      <Container className="min-h-screen flex-1  text-white sm:p-1 md:p-5 bg-gray-800">
+      <Container maxWidth={"lg"} className="min-h-screen flex-1 text-white sm:p-1 md:p-5 ">
         {children}
       </Container>
     </div>

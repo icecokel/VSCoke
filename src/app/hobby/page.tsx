77f0cdd53480e4242405a663d95acbf921ce320f
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-08-17 23:26:18 +0900

    임시 취미 라우팅 추가

---
+++ src/app/hobby/page.tsx
@@ -0,0 +1,12 @@
+import Hobby from "@/components/Hobby";
+import { NextPage } from "next";
+
+/**
+ * 취미 페이지
+ * @returns 취미 컴포넌트
+ */
+const HobbyPage: NextPage = () => {
+  return <Hobby />;
+};
+
+export default HobbyPage;

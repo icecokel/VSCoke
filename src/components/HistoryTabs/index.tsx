c43e61b7d4dae0957c690943df16410a36b76a97
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-08-18 09:27:52 +0900

    탭 닫기 기능 추가

--- src/components/HistoryTabs/index.tsx
+++ src/components/HistoryTabs/index.tsx
@@ -1,51 +1,57 @@
 "use client";
 
-import { IHistoryItem, historyAtom } from "@/atom/history";
+import { IHistoryItem } from "@/atom/history";
+import useHistory from "@/hooks/useHistory";
 import { IHaveChildren } from "@/models/common";
+import CloseIcon from "@mui/icons-material/Close";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import { useAtom } from "jotai/react";
-import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 const HistoryTabs = ({ children }: IHaveChildren) => {
-  const [history, setHistory] = useAtom(historyAtom);
-  const { push } = useRouter();
+  const { history, change, remove } = useHistory();
 
-  const handleClickTab = ({ isAactive, path, title }: IHistoryItem) => {
-    if (!isAactive) {
-      push(path);
-      setHistory(prev => prev.map(item => ({ ...item, isAactive: item.path === path })));
-    }
-  };
+  const handleClickTab = change;
+  const handleClickClose = remove;
 
   const handleRightClickTab = () => {
     console.log("우클릭");
   };
 
+  // TODO 드래그 기능 추가
+
   return (
     <div className="w-full bg-gray-800">
       <Stack direction={"row"} className="bg-gray-900">
-        {history.map(({ isAactive, path, title }) => (
-          <Tooltip key={`tab_${path}`} title={`${path}/${title}`}>
+        {history.map(item => (
+          <Tooltip key={`tab_${item.path}`} title={`${item.path}/${item.title}`}>
             <div
               className={twMerge(
                 "border border-gray-300/60 border-l-[0px]",
-                isAactive && "bg-gray-800 border-b-[0px]",
+                item.isAactive && "bg-gray-800 border-b-[0px]",
               )}
-              onClick={() => handleClickTab({ isAactive, path, title })}
+              onClick={() => handleClickTab(item)}
               onContextMenu={handleRightClickTab}
             >
               <Typography
                 className={twMerge(
-                  "text-gray-300/80 py-[6px] px-[20px]",
-                  isAactive && "text-yellow-200/95 font-medium border-t border-t-blue-100 pt-[5px]",
+                  "text-gray-300/80 md:py-[6px] md:px-[20px] py-[3px] px-[8px]",
+                  item.isAactive &&
+                    "text-yellow-200/95 font-medium border-t pt-[1px] border-t-blue-100 md:pt-[5px]",
                 )}
                 fontSize={14}
               >
-                {title}
+                {item.title}
+                <CloseIcon
+                  sx={{ fontSize: 15, fontWeight: 700 }}
+                  className={twMerge(
+                    "ml-[4px] mr-[-4px] md:ml-[8px] md:mr-[-8px]",
+                    !item.isAactive && "hidden",
+                  )}
+                  onClick={() => handleClickClose(item)}
+                />
               </Typography>
             </div>
           </Tooltip>

9b63412383ae32eaf095307482f4b18080abe43d
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-08-26 00:30:05 +0900

    노션이미지 제외

--- next.config.js
+++ next.config.js
@@ -12,8 +12,17 @@ const nextConfig = {
         port: "",
         pathname: "/images/*",
       },
+      {
+        protocol: "https",
+        hostname: "s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/*",
+      },
     ],
   },
 };
 
 module.exports = nextConfig;
+
+/*
+ */

1a5f5384fb32aad10e24630d6fdb11de3b33334f
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-02 11:44:17 +0900

    모바일 환경 대응

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,8 +5,9 @@ import Search from "./components/Search";
 import useClickOutSide from "@/hooks/useClickOutSide";
 import { IHaveChildren } from "@/models/common";
 import { TSidebar } from "@/models/enum/sidebar";
-import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
+import FileCopyOutlinedIcon from "@mui/icons-material/FileCopyOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import Fab from "@mui/material/Fab";
 import Stack from "@mui/material/Stack";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
@@ -14,7 +15,7 @@ import { twMerge } from "tailwind-merge";
 const TABS = [
   {
     name: "explore",
-    icon: <InsertDriveFileOutlinedIcon />,
+    icon: <FileCopyOutlinedIcon />,
   },
 
   {
@@ -43,7 +44,7 @@ const Sidebar = ({ children }: IHaveChildren) => {
           direction={"column"}
           gap={1}
           alignItems={"center"}
-          className="z-50 min-h-screen w-[60px] border-r-[1px] border-r-gray-500 bg-gray-900 py-2 text-gray-100"
+          className="md:flex hidden z-50 min-h-screen w-[60px] border-r-[1px] border-r-gray-500 bg-gray-900 py-2 text-gray-100"
         >
           {TABS.map(({ name, icon }) => {
             return (
@@ -51,7 +52,7 @@ const Sidebar = ({ children }: IHaveChildren) => {
                 key={`tab_${name}`}
                 className={twMerge(
                   "flex h-10 w-full cursor-pointer items-center justify-center border-l-2 bg-gray-900",
-                  name === tab ? "border-l-blue-100" : "border-l-gray-900",
+                  name === tab ? "border-l-blue-300" : "border-l-gray-900",
                 )}
               >
                 {icon}
@@ -69,6 +70,26 @@ const Sidebar = ({ children }: IHaveChildren) => {
         <Explorer isShowing={tab === "explore"} tabClose={tabClose} />
         <Search isShowing={tab === "search"} />
       </Stack>
+
+      <Stack className="absolute bottom-0 p-[10px] md:hidden flex" gap={1}>
+        {TABS.map(({ name, icon }) => {
+          return (
+            <Fab className="bg-gray-50" key={`tab_${name}`}>
+              <label>
+                {icon}
+                <input
+                  type="radio"
+                  name="tabs"
+                  defaultValue={name}
+                  className="hidden"
+                  onClick={handleChangeTab}
+                />
+              </label>
+            </Fab>
+          );
+        })}
+      </Stack>
+
       {children}
     </Stack>
   );

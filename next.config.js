2fff8fce3bb14075f4d3d96ee486d9ed5adbcb3e
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-08 22:04:17 +0900

    노션 이미지 경로 추가, 노션 라이브러리 추가

--- next.config.js
+++ next.config.js
@@ -18,11 +18,14 @@ const nextConfig = {
         port: "",
         pathname: "/secure.notion-static.com/**",
       },
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
 
 module.exports = nextConfig;
-
-/*
- */

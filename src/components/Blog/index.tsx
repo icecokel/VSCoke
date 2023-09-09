6a72a71cff0b6d876dc08e535aa3470ad51665a5
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-09 12:18:21 +0900

    포스트 목록 api 수정

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -10,6 +10,7 @@ interface IBlogProps {
   id: string;
 }
 
+// TODO 서버에서 호출 방안 고려
 const Blog = ({ id }: IBlogProps) => {
   const { data, isLoading } = useSWR(`https://notion-api.splitbee.io/v1/page/${id}`);
 

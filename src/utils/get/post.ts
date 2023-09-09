a298fe8abf2bb5b60a41be66b856da95e5d56c83
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-09 14:00:55 +0900

    post 데이터 취득 로직 수정

--- src/utils/get/post.ts
+++ src/utils/get/post.ts
@@ -2,10 +2,48 @@ import { ITree } from "@/components/Sidebar/components/Explorer";
 import notion from "@/service/notion";
 import { cache } from "react";
 
-export const revalidate = 3600; // revalidate the data at most every hour
+//
+
+// export const revalidate = 3600;
 
 export const getPost = cache(async (id: string) => {
-  const pageData = await notion.blocks.retrieve({ block_id: id?.toString() ?? "" });
-  const blockChildData = await notion.blocks.children.list({ block_id: id?.toString() ?? "" });
-  return { id, pageData, blockChildData };
+  const data = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`).then(res => res.json());
+
+  return {
+    blockMap: data,
+  };
+});
+
+export const getPosts = cache(async () => {
+  const database_id = process.env.NOTION_DATABASE_ID ?? "";
+  const response = await notion.databases.query({
+    database_id: database_id,
+    filter: {
+      or: [
+        {
+          property: "publicationAt",
+          date: {
+            equals: new Date().toISOString(),
+          },
+        },
+        {
+          property: "publicationAt",
+          date: {
+            before: new Date().toISOString(),
+          },
+        },
+      ],
+    },
+  });
+
+  const posts: ITree[] = response.results.map((post: any) => {
+    const url: string = post.url.slice(post.url.lastIndexOf("/") + 1);
+    return {
+      id: url,
+      label: post.properties.title.title[0].plain_text,
+      path: `/blog/${url}`,
+    };
+  });
+
+  return { id: "blog", label: "blog", items: posts };
 });

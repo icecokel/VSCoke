7cf8e2b4e4f0e16874a66b634bcb118a79c3a6fb
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-11 04:15:36 +0900

    주석 추가

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -6,6 +6,13 @@ interface IBlogPageProps {
   };
 }
 
+/**
+ * 블로그
+ *
+ * @param 블로그 url
+ * @returns 블로그 컴포넌트
+ */
+
 const BlogPage = ({ params }: IBlogPageProps) => {
   return <Blog id={params.id} />;
 };

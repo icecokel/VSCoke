c2904a587da06cbe46149d08818ff32d0c43259c
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-19 21:54:44 +0900

    다이얼 로그 추가

--- src/components/Hobby/Card.tsx
+++ src/components/Hobby/Card.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import Box from "@mui/material/Box";
+import Dialog, { DialogProps } from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 import Fade from "@mui/material/Fade";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
@@ -13,17 +16,21 @@ export interface ICard {
   date: string;
   title: string;
   review?: string;
-  items?: { imageSrc: string; review?: string }[];
+  items: string[];
 }
 
 const HobbyCard = ({ date, thumnail, title, items, review }: ICard) => {
   const [isHideInfo, setIsHideInfo] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHideInfo(false);
   };
   const handleMouseLeave = () => {
     setIsHideInfo(true);
+  };
+  const onToggleModal = () => {
+    setOpen(!open);
   };
 
   return (
@@ -32,6 +39,7 @@ const HobbyCard = ({ date, thumnail, title, items, review }: ICard) => {
         className="relative overflow-hidden rounded aspect-square p-[8px]"
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
+        onClick={onToggleModal}
       >
         <Stack
           alignItems={"center"}
@@ -54,21 +62,48 @@ const HobbyCard = ({ date, thumnail, title, items, review }: ICard) => {
             className="w-full h-full translate-y-[-100%] rounded bg-black/60"
           >
             <Box>
-              <Typography color={"white"} variant="h6">
+              <Typography color={"white"} variant="h6" fontWeight={600}>
                 {title}
               </Typography>
-              <Typography color={"white"} variant="body1">
+              <Typography variant="body1" className="text-white/80">
                 {date}
               </Typography>
-              <Typography color={"white"} variant="body2">
+              <Typography variant="body1" className="text-white/80">
                 {review}
               </Typography>
             </Box>
           </Stack>
         </Fade>
       </div>
+      <HobbyCard.detail open={open} items={items} onClose={onToggleModal} title={title} />
     </Grid>
   );
 };
 
 export default HobbyCard;
+
+interface HobbyCardDetailProps extends DialogProps {
+  title: string;
+  items?: string[];
+}
+
+HobbyCard.detail = ({ items, title, ...restProps }: HobbyCardDetailProps) => {
+  return (
+    <Dialog {...restProps}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        {items?.map(src => {
+          return (
+            <Image
+              src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${src}`}
+              width={300}
+              height={300}
+              alt=""
+              className="object-cover"
+            />
+          );
+        })}
+      </DialogContent>
+    </Dialog>
+  );
+};

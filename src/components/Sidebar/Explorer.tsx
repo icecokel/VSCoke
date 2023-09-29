1144f4ed9ffb33d393931dce4e541824479ba77f
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-09-29 11:05:56 +0900

    커스텀 훅 적용

--- src/components/Sidebar/Explorer.tsx
+++ src/components/Sidebar/Explorer.tsx
@@ -1,53 +1,15 @@
 "use client";
 
 import SidebarLayout from "./SidebarLayout";
-import { BlogContext } from "@/contexts/AppProvider";
+import useExplorer from "@/hooks/useExplorer";
+import { ITree } from "@/hooks/useExplorer";
 import useHistory from "@/hooks/useHistory";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import HikingRoundedIcon from "@mui/icons-material/HikingRounded";
-import PortraitRoundedIcon from "@mui/icons-material/PortraitRounded";
-import TextSnippetOutlinedIcon from "@mui/icons-material/TextSnippetOutlined";
 import TreeItem from "@mui/lab/TreeItem";
 import TreeView from "@mui/lab/TreeView";
 import LinearProgress from "@mui/material/LinearProgress";
 import Link from "next/link";
-import { ReactNode, useContext, useEffect, useState } from "react";
-
-export interface ITree {
-  id: string;
-  label: string;
-  icon?: ReactNode;
-  path?: string;
-  items?: ITree[];
-}
-
-const defaultData: ITree[] = [
-  {
-    id: "profile",
-    label: "profile",
-    items: [
-      {
-        id: "profile-index",
-        label: "Iam.html",
-        path: "/profile",
-        icon: <PortraitRoundedIcon className="text-yellow-200" />,
-      },
-    ],
-  },
-  {
-    id: "hobby",
-    label: "hobby",
-    items: [
-      {
-        id: "hobby-index",
-        label: "BackPacking.html",
-        path: "/hobby/BackPacking",
-        icon: <HikingRoundedIcon className="text-green-300" />,
-      },
-    ],
-  },
-];
 
 interface ExplorerProps {
   isShowing: boolean;
@@ -55,24 +17,7 @@ interface ExplorerProps {
 }
 
 const Explorer = ({ isShowing, tabClose }: ExplorerProps) => {
-  const [itemList, setItemList] = useState<ITree[]>(defaultData);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const blog = useContext(BlogContext);
-
-  useEffect(() => {
-    if (blog && !itemList.some(item => item.label === blog.label)) {
-      const addedIconBlog = { ...blog };
-      if (blog.items)
-        addedIconBlog.items = blog.items.map((item: ITree) => ({
-          ...item,
-          icon: <TextSnippetOutlinedIcon className="text-blue-100" />,
-        }));
-      setItemList(prev => [...prev, addedIconBlog]);
-      setIsLoading(false);
-    }
-  }, [blog]);
-
+  const { itemList, isLoading } = useExplorer();
   return (
     <SidebarLayout isShowing={isShowing}>
       {isLoading ? (

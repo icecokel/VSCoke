459a5f8784fa1287feab528f22c596b2143d2a48
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-10-12 08:05:21 +0900

    트리 데이터 취득 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import HistoryTabs from "@/components/HistoryTabs";
 import Sidebar from "@/components/Sidebar";
 import AppProvider from "@/contexts/AppProvider";
 import { IHaveChildren } from "@/models/common";
+import { getExplorer } from "@/utils/get/explorer";
 import { getPosts } from "@/utils/get/post";
 import { Metadata } from "next";
 import "prismjs/themes/prism-tomorrow.css";
@@ -16,10 +17,12 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children }: IHaveChildren) {
   const blogs = await getPosts();
+  const explorer = await getExplorer();
+
   return (
     <html lang="ko">
       <body>
-        <AppProvider blogs={blogs}>
+        <AppProvider blogs={blogs} explorer={explorer}>
           <Sidebar>
             <HistoryTabs>{children}</HistoryTabs>
           </Sidebar>
--- src/hooks/useExplorer.tsx
+++ src/hooks/useExplorer.tsx
@@ -1,68 +1,35 @@
 "use client";
 
-import { BlogContext } from "@/contexts/AppProvider";
-import HikingRoundedIcon from "@mui/icons-material/HikingRounded";
-import PortraitRoundedIcon from "@mui/icons-material/PortraitRounded";
-import TextSnippetOutlinedIcon from "@mui/icons-material/TextSnippetOutlined";
-import { ReactNode, useContext, useEffect, useState } from "react";
+import { BlogContext, ExplorerContext } from "@/contexts/AppProvider";
+import { useContext, useEffect, useState } from "react";
 
 export interface ITree {
   id: string;
   label: string;
-  icon?: ReactNode;
+  icon?: string;
   path?: string;
   items?: ITree[];
 }
 
-const iconMap = {
-  profile: <PortraitRoundedIcon className="text-yellow-200" />,
-  hobby: <HikingRoundedIcon className="text-green-300" />,
-  blog: <TextSnippetOutlinedIcon className="text-blue-100" />,
-};
-
-const defaultData: ITree[] = [
-  {
-    id: "profile",
-    label: "profile",
-    items: [
-      {
-        id: "profile-index",
-        label: "Iam.html",
-        path: "/profile",
-        icon: <PortraitRoundedIcon className="text-yellow-200" />,
-      },
-    ],
-  },
-  {
-    id: "hobby",
-    label: "hobby",
-    items: [
-      {
-        id: "hobby-index",
-        label: "BackPacking.html",
-        path: "/hobby/BackPacking",
-        icon: <HikingRoundedIcon className="text-green-300" />,
-      },
-    ],
-  },
-];
-
 const useExplorer = () => {
-  const [itemList, setItemList] = useState<ITree[]>(defaultData);
+  const [itemList, setItemList] = useState<ITree[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const explorer = useContext(ExplorerContext);
   const blog = useContext(BlogContext);
+
   useEffect(() => {
-    if (blog && !itemList.some(item => item.label === blog.label)) {
+    if (explorer && blog && !itemList.some(item => item.label === blog.label)) {
       const addedIconBlog = { ...blog };
       if (blog.items)
         addedIconBlog.items = blog.items.map((item: ITree) => ({
           ...item,
-          icon: <TextSnippetOutlinedIcon className="text-blue-100" />,
+          icon: "blog",
         }));
-      setItemList(prev => [...prev, addedIconBlog]);
+
+      setItemList([...explorer, addedIconBlog]);
       setIsLoading(false);
     }
-  }, [blog]);
+  }, [explorer, blog]);
 
   return {
     isLoading,

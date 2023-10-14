25ecb8e9ef3a39ff449b9f21d2351a06c3c74685
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-10-14 11:46:04 +0900

    프로바이다 수정

--- src/contexts/AppProvider.tsx
+++ src/contexts/AppProvider.tsx
@@ -2,21 +2,25 @@
 
 import MuiConfigProvider from "./MuiConfigProvider";
 import { SWRProvider } from "./SWRProvider";
-import { ITree } from "@/components/Sidebar/Explorer";
+import { ITree } from "@/hooks/useExplorer";
 import { IHaveChildren } from "@/models/common";
 import { createContext } from "react";
 
 export const BlogContext = createContext<ITree>({} as ITree);
+export const ExplorerContext = createContext<ITree[]>([]);
 
 interface IAppProviderProps extends IHaveChildren {
   blogs: ITree;
+  explorer: ITree[];
 }
 
-const AppProvider = ({ children, blogs }: IAppProviderProps) => {
+const AppProvider = ({ children, blogs, explorer }: IAppProviderProps) => {
   return (
     <SWRProvider>
       <MuiConfigProvider>
-        <BlogContext.Provider value={blogs}>{children}</BlogContext.Provider>
+        <ExplorerContext.Provider value={explorer}>
+          <BlogContext.Provider value={blogs}>{children}</BlogContext.Provider>
+        </ExplorerContext.Provider>
       </MuiConfigProvider>
     </SWRProvider>
   );

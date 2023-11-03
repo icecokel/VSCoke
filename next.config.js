07264e392006b46499dcb3cf8579e5dcc2f2c32c
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-03 21:30:20 +0900

    git pages 배포 테스트 중

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "VSCoke";
+
 const nextConfig = {
   reactStrictMode: false,
+  basePath: "/VSCoke",
+  assetPrefix: !debug ? `/${repository}/` : "",
+  trailingSlash: true,
+  output: "export",
   experimental: {
     appDir: true,
   },

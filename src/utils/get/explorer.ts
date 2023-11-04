c1f8cfc6985107c5e95c84932b8a0b3e4655d122
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-04 19:46:11 +0900

    노션 제거

--- src/utils/get/explorer.ts
+++ src/utils/get/explorer.ts
@@ -2,7 +2,18 @@ import { getData } from "@/service/firebase/service";
 import { cache } from "react";
 
 export const getExplorer = cache(async () => {
-  const data = await getData("vscoke", "explorer");
+  // const data = await getData("vscoke", "explorer");
+  // console.log(data);
+
+  const data = {
+    tree: [
+      {
+        id: "profile",
+        label: "profile",
+        items: [{ icon: "profile", id: "profile-index", label: "Iam.html", path: "/profile" }],
+      },
+    ],
+  };
 
   if (!data) {
     return [];

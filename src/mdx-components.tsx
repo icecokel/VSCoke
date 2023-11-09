9d6cf7fdc7d1e0745341f3252b1a7e79469f8e69
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-09 21:10:51 +0900

    컴포넌트 스타일 수정

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,12 +2,35 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <h1 style={{ fontSize: "72px", fontWeight: 700 }}>{children}</h1>,
-    h2: ({ children }) => <h1 style={{ fontSize: "68px", fontWeight: 700 }}>{children}</h1>,
-    h3: ({ children }) => <h1 style={{ fontSize: "60px", fontWeight: 700 }}>{children}</h1>,
-    h4: ({ children }) => <h1 style={{ fontSize: "42px", fontWeight: 700 }}>{children}</h1>,
-    h5: ({ children }) => <h1 style={{ fontSize: "28px", fontWeight: 700 }}>{children}</h1>,
-    h6: ({ children }) => <h1 style={{ fontSize: "24px", fontWeight: 700 }}>{children}</h1>,
+    h1: ({ children }) => (
+      <h3
+        style={{
+          fontSize: "30x",
+          fontWeight: 600,
+          padding: "3px 2px",
+          marginTop: "32px",
+          marginBottom: "4px",
+        }}
+      >
+        {children}
+      </h3>
+    ),
+    h2: ({ children }) => (
+      <h2
+        style={{
+          fontSize: "24px",
+          fontWeight: 600,
+          padding: "3px 2px",
+          marginTop: "22px",
+          marginBottom: "1px",
+        }}
+      >
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 style={{ fontSize: "20px", fontWeight: 600, padding: "3px 2px" }}>{children}</h3>
+    ),
     ul: ({ children }) => <ul style={{ listStyle: "disc", paddingLeft: "2em" }}>{children}</ul>,
     blockquote: ({ children }) => (
       <div style={{ borderLeft: "0.25em solid gray", padding: "1em" }}>{children}</div>

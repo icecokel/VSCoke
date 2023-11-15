c48331fa88ac924c7d9033b5eeb412b07e6d2fd9
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-15 22:52:01 +0900

    mdx 제거

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const withMDX = require("@next/mdx")();
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
   basePath: "/VSCoke",
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -31,4 +29,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;

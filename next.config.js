0db31076e63dfbc36361a5d6ada5e2ece52c61ab
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-16 23:21:02 +0900

    컨텐츠 레이어 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const { withContentlayer } = require("next-contentlayer");
+
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
@@ -29,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);

e1a9dd934ff4ef853c951b58354dee5f7c4f0f56
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-19 01:12:41 +0900

    posts 정보 취득 로직 수정

--- src/utils/get/post.ts
+++ src/utils/get/post.ts
@@ -1,10 +1,7 @@
 import { ITree } from "@/hooks/useExplorer";
-import notion from "@/service/notion";
+import { allPosts } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
 import { cache } from "react";
-
-//
-
-// export const revalidate = 3600;
 
 export const getPost = cache(async (id: string) => {
   const data = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`).then(res => res.json());
@@ -15,34 +12,9 @@ export const getPost = cache(async (id: string) => {
 });
 
 export const getPosts = cache(async () => {
-  const database_id = process.env.NOTION_DATABASE_ID ?? "";
-  const response = await notion.databases.query({
-    database_id: database_id,
-    filter: {
-      or: [
-        {
-          property: "publicationAt",
-          date: {
-            equals: new Date().toISOString(),
-          },
-        },
-        {
-          property: "publicationAt",
-          date: {
-            before: new Date().toISOString(),
-          },
-        },
-      ],
-    },
-  });
-
-  const posts: ITree[] = response.results.map((post: any) => {
-    const url: string = post.url.slice(post.url.lastIndexOf("/") + 1);
-    return {
-      id: url,
-      label: post.properties.title.title[0].plain_text,
-      path: `/blog/${url}`,
-    };
+  const result = allPosts.sort((a: any, b: any) => compareDesc(new Date(a.date), new Date(b.date)));
+  const posts: ITree[] = result.map(post => {
+    return { id: post._raw.flattenedPath, label: post.title, path: post.url };
   });
 
   return { id: "blog", label: "blog", items: posts };

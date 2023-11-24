79a5b722183e46b2e661369357666652faa59a39
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-24 22:07:04 +0900

    코드블럭 스타일 수정

--- src/components/mdx/MdxCodeBlock.tsx
+++ src/components/mdx/MdxCodeBlock.tsx
@@ -7,10 +7,11 @@ export interface IMdxCodeblockProps {
   code: string;
 }
 
+// TODO type에 따라 스타일링
 const MdxCodeBlock = ({ code, type }: IMdxCodeblockProps) => {
   return (
-    <pre className="p-[1em] bg-beige-400 rounded my-[1em]">
-      <code className="text-[14px] leading-[1.5em] text-black">{code}</code>
+    <pre className="p-[2em] bg-beige-400 rounded my-[1em]">
+      <code className="text-[14px] leading-[2em] text-black">{code}</code>
     </pre>
   );
 };

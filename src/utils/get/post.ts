d47ed70af81b03725b920327f523485a44d61a90
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-25 23:03:24 +0900

    notion, firebase 제거, 사용처 정리

--- src/utils/get/post.ts
+++ src/utils/get/post.ts
@@ -3,14 +3,6 @@ import { allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 import { cache } from "react";
 
-export const getPost = cache(async (id: string) => {
-  const data = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`).then(res => res.json());
-
-  return {
-    blockMap: data,
-  };
-});
-
 export const getPosts = cache(async () => {
   const result = allPosts.sort((a: any, b: any) => compareDesc(new Date(a.date), new Date(b.date)));
   const posts: ITree[] = result.map(post => {

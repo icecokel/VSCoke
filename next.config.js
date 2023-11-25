d58d7b8211eef51e367115d3dc578771ba8c2205
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-25 09:58:39 +0900

    path 수정

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
-  basePath: "/",
   output: "export",
   images: {
     unoptimized: true,

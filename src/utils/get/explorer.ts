3ea91d3542b407d750914835c4cd829cc54b3e24
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2023-11-25 23:03:24 +0900

    notion, firebase 제거, 사용처 정리

--- src/utils/get/explorer.ts
+++ src/utils/get/explorer.ts
@@ -1,10 +1,6 @@
-import { getData } from "@/service/firebase/service";
 import { cache } from "react";
 
 export const getExplorer = cache(async () => {
-  // const data = await getData("vscoke", "explorer");
-  // console.log(data);
-
   const data = {
     tree: [
       {

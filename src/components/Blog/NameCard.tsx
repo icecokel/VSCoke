bb010bedc616381ead1d38105611816d53bbd1f9
icecokel <red9runge@gmail.com>
2023-12-13 13:58:55 +0900

    change : 링크 형식 수정, 히스토리 탭 사용하도록

--- src/components/Blog/NameCard.tsx
+++ src/components/Blog/NameCard.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import useHistory from "@/hooks/useHistory";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 
 const NameCard = () => {
+  const { add } = useHistory();
+  const handleClickLink = () => {
+    add({ path: "/profile", isActive: true, title: "profile" });
+  };
   return (
-    <Link href={"/profile"}>
-      <div className="flex gap-5 items-center border-t-[1px] p-[20px] border-t-gray-500">
+    <div className="border-t-[1px] p-[20px] border-t-gray-500">
+      <button className="flex gap-5 items-center" onClick={handleClickLink}>
         <Avatar
           className="h-[120px] w-[120px] border-2 border-gray-300"
           src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}profileImg.jpg`}
@@ -18,8 +23,8 @@ const NameCard = () => {
             소심한 관종 개발자
           </Typography>
         </div>
-      </div>
-    </Link>
+      </button>
+    </div>
   );
 };
 

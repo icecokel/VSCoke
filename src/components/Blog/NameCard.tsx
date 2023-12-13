9d72579162877e0231c859eb0032f6ce763a1fdd
icecokel <red9runge@gmail.com>
2023-12-13 13:49:48 +0900

    feature : namecard 문구 수정, 링크 추가

--- src/components/Blog/NameCard.tsx
+++ src/components/Blog/NameCard.tsx
@@ -2,18 +2,24 @@
 
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 const NameCard = () => {
   return (
-    <div className="flex gap-5 items-center border-t-[1px] p-[20px] border-t-gray-500">
-      <Avatar
-        className="h-[120px] w-[120px] border-4 border-yellow-200"
-        src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}profileImg.jpg`}
-      />
-      <div>
-        <Typography variant="h6">얼음콜라의 블로그</Typography>
+    <Link href={"/profile"}>
+      <div className="flex gap-5 items-center border-t-[1px] p-[20px] border-t-gray-500">
+        <Avatar
+          className="h-[120px] w-[120px] border-2 border-gray-300"
+          src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}profileImg.jpg`}
+        />
+        <div>
+          <Typography variant="h6">얼음콜라</Typography>
+          <Typography variant="body2" color={"text.secondary"}>
+            소심한 관종 개발자
+          </Typography>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

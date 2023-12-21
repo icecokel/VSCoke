ec20a4251c7ae057781bb3eb2ae7a270284c7479
icecokel <red9runge@gmail.com>
2023-12-21 22:51:49 +0900

    feature : 발행일 필터 추가

--- src/utils/get/post.ts
+++ src/utils/get/post.ts
@@ -5,9 +5,12 @@ import { cache } from "react";
 
 export const getPosts = cache(async () => {
   const result = allPosts.sort((a: any, b: any) => compareDesc(new Date(a.date), new Date(b.date)));
-  const posts: ITree[] = result.map(post => {
-    return { id: post._raw.flattenedPath, label: post.title, path: post.url };
-  });
+  const today = new Date();
+  const posts: ITree[] = result
+    .filter(item => new Date(item.date).getTime() <= today.getTime())
+    .map(post => {
+      return { id: post._raw.flattenedPath, label: post.title, path: post.url };
+    });
 
   return { id: "blog", label: "blog", items: posts };
 });

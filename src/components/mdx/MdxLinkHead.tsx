b81d20ef71e96e0828947bd56051d24202aad0fe
icecokel <red9runge@gmail.com>
2023-12-25 11:36:04 +0900

    bugfix : 포스팅 마다 네이게이션 다르게 설정

--- src/components/mdx/MdxLinkHead.tsx
+++ src/components/mdx/MdxLinkHead.tsx
@@ -2,6 +2,7 @@
 
 import { mdxContext } from "./MdxContext";
 import { IHaveChildren } from "@/models/common";
+import { usePathname } from "next/navigation";
 import { useContext, useEffect } from "react";
 
 export type TVariant = "h1" | "h2" | "h3";
@@ -32,12 +33,13 @@ export const PREFIX = "link-title";
 
 const MdxLinkHead = ({ children, variant }: MdxLinkHeadProps) => {
   const { add } = useContext(mdxContext);
+  const pathname = usePathname();
 
   const id = `${PREFIX}-${(children as string[])[1]}`;
 
-  // TODO 타입 개선
+  // // TODO 타입 개선
   useEffect(() => {
-    add({ type: variant, label: (children as string[])[1] });
+    add({ title: pathname, items: [{ type: variant, label: (children as string[])[1] }] });
   }, []);
   return (
     <h1 id={id} style={STYLE_MAP[variant]}>

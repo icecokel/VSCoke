ae460b05568fb931b1fa9e6d20a87d2506bb9706
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-01-16 07:44:43 +0900

    feature : chip 컴포넌트 추가 및 적용

---
+++ src/components/baseUi/Chip.tsx
@@ -0,0 +1,27 @@
+import BaseText from "./Text";
+
+interface IChipProps {
+  label: string;
+  className: string;
+}
+
+const Chip = ({ label, className }: IChipProps) => {
+  return (
+    <div
+      className={className}
+      style={{
+        display: "inline-flex",
+        border: "1px solid white",
+        borderRadius: "15px",
+        padding: "0.25em 0.5em",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "0.25em",
+      }}
+    >
+      <BaseText type="caption">{label}</BaseText>
+    </div>
+  );
+};
+
+export default Chip;

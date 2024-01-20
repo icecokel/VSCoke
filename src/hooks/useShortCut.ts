56a10cbf249c8dee28ce8e1c4c307229923226f7
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-01-20 11:57:02 +0900

    refactor :  단축키 파라 미터 변경, 탐색키 단축키 추가

--- src/hooks/useShortCut.ts
+++ src/hooks/useShortCut.ts
@@ -1,11 +1,10 @@
 import useKeyPress from "@/hooks/useKeyPress";
 import { useEffect } from "react";
 
-const useShortCut = (keys: string[], cb: Function) => {
+const useShortCut = (keys: KeyboardEvent["key"][], cb: Function) => {
   const { pushedKey, isSubset } = useKeyPress();
 
   useEffect(() => {
-    // TODO 맥일때 확인
     if (isSubset(keys)) {
       cb();
     }

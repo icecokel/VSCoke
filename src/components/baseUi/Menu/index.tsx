ea5956739313f6696814072b61cf3b4b391cc50e
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-01-26 08:00:04 +0900

    feature : Menu 컴포넌트 생성  및 적용

---
+++ src/components/baseUi/Menu/index.tsx
@@ -0,0 +1,47 @@
+import useClickOutSide from "@/hooks/useClickOutSide";
+import { IHaveChildren } from "@/models/common";
+
+interface IMenuProps extends IHaveChildren {
+  targetEl: null | HTMLElement;
+  onClose: () => void;
+}
+
+const Menu = ({ targetEl, children, onClose }: IMenuProps) => {
+  const open = Boolean(targetEl);
+  const ref = useClickOutSide(onClose);
+
+  if (!open) {
+    return <></>;
+  }
+
+  const rect = targetEl?.getBoundingClientRect();
+  return (
+    <ul
+      style={{
+        top: rect?.bottom,
+        left: rect?.left,
+      }}
+      className="absolute min-w-[10em] p-2 bg-gray-800 text-white rounded gap-2 flex flex-col border-gray-900 z-50"
+      ref={ref}
+    >
+      {children}
+    </ul>
+  );
+};
+
+export default Menu;
+
+interface IMenuItemProps extends IHaveChildren {
+  onClick?: () => void;
+}
+
+Menu.item = ({ onClick, children }: IMenuItemProps) => {
+  return (
+    <li
+      onClick={onClick}
+      className="hover:bg-white/20 text-sm p-1 hover:text-yellow-200 hover:font-bold z-50 cursor-pointer"
+    >
+      {children}
+    </li>
+  );
+};

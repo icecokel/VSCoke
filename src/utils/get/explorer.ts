8339f9461f517be49056f714c93019d9b332dd11
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-02-08 22:10:05 +0900

    faeture : tree 스타일 수정, package.json 추가

--- src/utils/get/explorer.ts
+++ src/utils/get/explorer.ts
@@ -10,6 +10,11 @@ export const getExplorer = cache(async (): Promise<ITree[]> => {
       items: [{ icon: "profile", id: "profile-index", label: "Iam.html", path: "/profile" }],
     },
     {
+      index: 98,
+      id: "package.json",
+      label: "package.json",
+    },
+    {
       index: 99,
       id: "readme",
       label: "README.md",

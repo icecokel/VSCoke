db6828367fa0db3b22f6c3ae15d64fdb6dea2b88
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-02-11 21:44:54 +0900

    feature : modal dialog 분리

--- src/components/baseUi/Dialog.tsx
+++ src/components/baseUi/Dialog.tsx
@@ -1,33 +1,22 @@
 import Icon from "./Icon";
+import Modal, { IModalProps } from "./Modal";
 import BaseText from "./Text";
-import useClickOutSide from "@/hooks/useClickOutSide";
-import { IHaveChildren } from "@/models/common";
-import { createPortal } from "react-dom";
 
-interface IModalProps extends IHaveChildren {
+interface IDialogProps extends IModalProps {
   open: boolean;
   title?: string;
   onClose: () => void;
 }
 
-const Dialog = ({ open, title, children, onClose }: IModalProps) => {
-  const ref = useClickOutSide(onClose);
+const Dialog = ({ open, title, children, onClose }: IDialogProps) => {
   return (
-    <>
-      {open &&
-        createPortal(
-          <div className="fixed bg-gray-900/60 inset-0 flex items-center justify-center">
-            <div className="flex flex-col bg-white p-3 rounded min-w-[200px]" ref={ref}>
-              <div className="flex justify-between items-center mb-3">
-                {title && <BaseText type="h6">{title}</BaseText>}
-                <Icon kind="close" className="text-gray-500" />
-              </div>
-              <div>{children}</div>
-            </div>
-          </div>,
-          document.body,
-        )}
-    </>
+    <Modal open={open} onClose={onClose}>
+      <div className="flex justify-between items-center mb-3">
+        {title && <BaseText type="h6">{title}</BaseText>}
+        <Icon kind="close" className="text-gray-500" />
+      </div>
+      <div>{children}</div>
+    </Modal>
   );
 };
 

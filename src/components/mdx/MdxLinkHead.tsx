886a9e1789d4f0090a160baa6e01586ecdfc1111
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-02-27 00:08:34 +0900

    헤더 스티키 스타일 추가

--- src/components/mdx/MdxLinkHead.tsx
+++ src/components/mdx/MdxLinkHead.tsx
@@ -3,7 +3,7 @@
 import { mdxContext } from "../../contexts/MdxContext";
 import { IHaveChildren } from "@/models/common";
 import { usePathname } from "next/navigation";
-import { useContext, useEffect } from "react";
+import { CSSProperties, createElement, useContext, useEffect } from "react";
 
 export type TVariant = "h1" | "h2" | "h3";
 
@@ -11,22 +11,39 @@ interface MdxLinkHeadProps extends IHaveChildren {
   variant: TVariant;
 }
 
-const STYLE_MAP: { [key in TVariant]: object } = {
+const STYLE_MAP: { [key in TVariant]: CSSProperties } = {
   h1: {
     fontSize: "30px",
     fontWeight: 600,
     padding: "3px 2px",
+    height: "45px",
     marginTop: "1em",
     marginBottom: "4px",
+    position: "sticky",
+    top: "4px",
+    backgroundColor: "#FFFFFF",
   },
   h2: {
     fontSize: "24px",
     fontWeight: 600,
     padding: "3px 2px",
+    height: "45px",
     marginTop: "1em",
     marginBottom: "1px",
+    position: "sticky",
+    top: "4px",
+    backgroundColor: "#FFFFFF",
   },
-  h3: { fontSize: "20px", fontWeight: 600, marginTop: "1em", padding: "3px 2px" },
+  h3: {
+    fontSize: "20px",
+    fontWeight: 600,
+    marginTop: "1em",
+    padding: "3px 2px",
+    height: "45px",
+    position: "sticky",
+    top: "4px",
+    backgroundColor: "#FFFFFF",
+  },
 };
 
 export const PREFIX = "link-title";
@@ -41,11 +58,12 @@ const MdxLinkHead = ({ children, variant }: MdxLinkHeadProps) => {
   useEffect(() => {
     add({ title: pathname, items: [{ type: variant, label: (children as string[])[1] }] });
   }, []);
-  return (
-    <h1 id={`${id}`} style={STYLE_MAP[variant]}>
-      <a href={`#${id}`}>{children}</a>
-    </h1>
-  );
+
+  return createElement(variant, {
+    id: `${id}`,
+    style: STYLE_MAP[variant],
+    children: <a href={`#${id}`}>{children}</a>,
+  });
 };
 
 export default MdxLinkHead;

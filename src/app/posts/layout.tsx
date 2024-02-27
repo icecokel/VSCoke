e70cfc865968c7224e72a20d0daacea4bf9d793f
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-02-28 08:22:07 +0900

    post 진행도 추가

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -4,11 +4,43 @@ import MdxNav from "@/components/mdx/MdxNav";
 import MdxProvider from "@/contexts/MdxContext";
 import { IHaveChildren } from "@/models/common";
 import Container from "@ui/Container";
+import { useEffect, useRef, useState } from "react";
 
 export default function Layout({ children }: IHaveChildren) {
+  const [readPerPost, setReadPerPost] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  // TODO - 디바운스 추가 스타일링
+  const handleScrollPost = (postHeight: number) => () => {
+    if (window.scrollY > 0) {
+      const computed = (window.scrollY / postHeight) * 100;
+      setReadPerPost(prev => {
+        if (computed >= 100) {
+          return 100;
+        }
+        return Math.round((window.scrollY / postHeight) * 100);
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (window && ref.current) {
+      const event = handleScrollPost(ref.current?.clientHeight - window.innerHeight);
+      window.addEventListener("scroll", event);
+      return () => {
+        window.removeEventListener("scroll", event);
+      };
+    }
+  }, []);
+
   return (
     <MdxProvider>
-      <div className="flex gap-2">
+      <progress
+        className="w-full h-1 sticky top-0 px-[1em] md:px[2em] rounded-md"
+        max={100}
+        value={readPerPost}
+      >{`${readPerPost}%`}</progress>
+      <div className="flex gap-2" ref={ref}>
         <Container
           maxWidth="md"
           className="px-[1em] md:px[2em] w-full bg-white text-black/80 rounded-md"

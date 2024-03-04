590ccc1995b4214ee442b9932425b8188b9fd436
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-03-05 08:09:54 +0900

    스낵바 디자인 수정, 스낵바 적용

--- src/components/baseUi/SnackBar/index.tsx
+++ src/components/baseUi/SnackBar/index.tsx
@@ -18,13 +18,12 @@ const SnackBar = () => {
     }
   }, [open]);
 
-  // TODO 디자인 다시 고민
   return (
     <>
       {open &&
         createPortal(
           <div className="fixed inset-x-0 top-10 flex justify-center z-40">
-            <div className="border-2 border-beige-400 bg-beige-400/90 h-10 p-5 flex items-center justify-center rounded shadow-[4px_6px_6px_rgb(0,0,0,0.6)]">
+            <div className="bg-blue-100/90 h-10 p-5 flex items-center justify-center rounded shadow-[4px_6px_6px_rgb(0,0,0,0.6)]">
               <BaseText type="body1" className="text-gray-900 font-bold">
                 {message}
               </BaseText>

4e2e84e37a915655913ac41648fb92be966a8d62
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-03-07 21:53:01 +0900

    스낵바 위치 조절

--- src/components/baseUi/SnackBar/index.tsx
+++ src/components/baseUi/SnackBar/index.tsx
@@ -22,7 +22,7 @@ const SnackBar = () => {
     <>
       {open &&
         createPortal(
-          <div className="fixed inset-x-0 top-10 flex justify-center z-40">
+          <div className="fixed inset-x-0 top-[100px] flex justify-center z-40">
             <div className="bg-blue-100/90 h-10 p-5 flex items-center justify-center rounded shadow-[4px_6px_6px_rgb(0,0,0,0.6)]">
               <BaseText type="body1" className="text-gray-900 font-bold">
                 {message}

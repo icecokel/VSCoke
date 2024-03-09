17ae0bbff6447831f3a77989054a36e1e510d29a
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-03-09 23:48:07 +0900

    네임 카드 툴팁 추가

--- src/components/Blog/NameCard.tsx
+++ src/components/Blog/NameCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Tooltip from "../baseUi/Tooltip";
 import useHistory from "@/hooks/useHistory";
 import Avatar from "@ui/Avatar";
 import BaseText from "@ui/Text";
@@ -11,17 +12,19 @@ const NameCard = () => {
   };
   return (
     <div className="p-5 bg-blue-300/20 rounded-md mt-10">
-      <button className="flex gap-5 items-center" onClick={handleClickLink}>
-        <Avatar
-          size={120}
-          className="h-[120px] w-[120px] border-2 border-gray-300"
-          src={"profileImg.jpg"}
-        />
-        <div className="flex flex-col items-start">
-          <BaseText type="h6">얼음콜라</BaseText>
-          <BaseText type="body2">소심한 관종 개발자</BaseText>
-        </div>
-      </button>
+      <Tooltip text="이력서 보러가기">
+        <button className="flex gap-5 items-center" onClick={handleClickLink}>
+          <Avatar
+            size={120}
+            className="h-[120px] w-[120px] border-2 border-gray-300"
+            src={"profileImg.jpg"}
+          />
+          <div className="flex flex-col items-start">
+            <BaseText type="h6">얼음콜라</BaseText>
+            <BaseText type="body2">소심한 관종 개발자</BaseText>
+          </div>
+        </button>
+      </Tooltip>
     </div>
   );
 };

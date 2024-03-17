7015d240e423f46bb716d9af6a002e547aa9fdb5
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-03-17 19:33:06 +0900

    아코디언 컴포넌트 기능 구성

--- src/components/baseUi/Accordion/index.tsx
+++ src/components/baseUi/Accordion/index.tsx
@@ -1,17 +1,62 @@
-import { IHaveChildren } from "@/models/common";
+"use client";
 
-interface IAccordionProps extends IHaveChildren {
+import Icon from "../Icon";
+import Slide from "../Slide";
+import BaseText from "../Text";
+import { IHaveChildren } from "@/models/common";
+import { createContext, useContext, useState } from "react";
+
+interface IAccordion {
   expanded: boolean;
+  toggle: () => void;
 }
 
-const Accordion = ({ children }: IAccordionProps) => {
-  return <div className="p-5">{children}</div>;
+interface IAccordionProps extends IHaveChildren {}
+
+const DEFAULT_VALUE: IAccordion = {
+  expanded: false,
+  toggle: () => {},
 };
 
+const AccordionContext = createContext<IAccordion>(DEFAULT_VALUE);
+
+const Accordion = ({ children }: IAccordionProps) => {
+  const [expanded, setExpanded] = useState<boolean>(DEFAULT_VALUE.expanded);
+
+  const onToggle = () => {
+    setExpanded(prev => !prev);
+  };
+  return (
+    <div className="p-5">
+      <AccordionContext.Provider value={{ expanded, toggle: onToggle }}>
+        {children}
+      </AccordionContext.Provider>
+    </div>
+  );
+};
+
+export default Accordion;
+
 Accordion.Summary = ({ children }: IHaveChildren) => {
-  return <div>{children}</div>;
+  const { expanded, toggle } = useContext(AccordionContext);
+  const handleClickToggle = toggle;
+  return (
+    <button
+      className="flex items-center justify-between border border-gray-500 rounded p-2 w-full"
+      onClick={handleClickToggle}
+    >
+      <BaseText type="h6">{children}</BaseText>
+      <Icon kind={expanded ? "expand_more" : "chevron_right"} />
+    </button>
+  );
 };
 
 Accordion.Details = ({ children }: IHaveChildren) => {
-  return <div>{children}</div>;
+  const { expanded } = useContext(AccordionContext);
+
+  return (
+    <Slide active={expanded} direction="down" duration={100}>
+      <div className="p-2 border border-gray-500 rounded mt-2">{children}</div>
+    </Slide>
+  );
 };

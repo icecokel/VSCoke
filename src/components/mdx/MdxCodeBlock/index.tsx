6d75113c6ae6500c2751e6a4e862c691378da314
icecokel <red9runge@gmail.com>
2024-03-25 21:25:02 +0900

    코드 블럭 테일윈드 제거

--- src/components/mdx/MdxCodeBlock/index.tsx
+++ src/components/mdx/MdxCodeBlock/index.tsx
@@ -1,3 +1,5 @@
+import styles from "./style.module.css";
+
 export type TCodeBlockType = "js" | "jsx" | "ts" | "tsx" | "html" | "xml";
 
 export interface IMdxCodeblockProps {
@@ -8,8 +10,8 @@ export interface IMdxCodeblockProps {
 // TODO type에 따라 스타일링
 const MdxCodeBlock = ({ code, type }: IMdxCodeblockProps) => {
   return (
-    <pre className="p-[1em] md:p-[2em] bg-beige-400 rounded my-[2em] whitespace-pre-wrap break-words">
-      <code className="text-[14px] leading-[2em] text-black">{code}</code>
+    <pre className={styles.wrapper}>
+      <code className={styles.code}>{code}</code>
     </pre>
   );
 };

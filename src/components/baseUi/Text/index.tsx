2243fea7f978074d64ddc71253defd3eb8740897
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-04-06 20:30:04 +0900

    텍스트 테일윈드 제거

--- src/components/baseUi/Text/index.tsx
+++ src/components/baseUi/Text/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { styles } from "./style";
+import styles from "./style.module.css";
 import { TTextType } from "./type";
 import { HTMLAttributes, createElement } from "react";
 
@@ -8,11 +8,11 @@ interface IBaseTextProps extends HTMLAttributes<HTMLSpanElement> {
   type?: TTextType;
 }
 
-const BaseText = ({ children, type = "body1", style, ...props }: IBaseTextProps) => {
+const BaseText = ({ children, type = "body1", ...props }: IBaseTextProps) => {
   if (!["body1", "body2", "caption"].includes(type)) {
-    return createElement(type, { style: { ...styles[type], ...style }, ...props }, children);
+    return createElement(type, { className: styles[type], ...props }, children);
   }
-  return createElement("span", { style: { ...styles[type], ...style }, ...props }, children);
+  return createElement("span", { className: styles[type], ...props }, children);
 };
 
 export default BaseText;

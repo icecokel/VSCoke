3755878b791911a8e85d0be0b443e0ac0fafcf7d
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-04-14 20:10:21 +0900

    화살표 위치 자동으로 선정되도록 수정

--- src/components/mdx/MdxSchematic/index.tsx
+++ src/components/mdx/MdxSchematic/index.tsx
@@ -2,16 +2,18 @@
 
 import Icon from "@/components/baseUi/Icon";
 import BaseText from "@/components/baseUi/Text";
-import { IHaveChildren } from "@/models/common";
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 
-export interface MdxSchematicProps extends IHaveChildren {}
+export interface MdxSchematicProps {
+  items: IItemProps[];
+}
 
-const MdxSchematic = ({}: MdxSchematicProps) => {
+const MdxSchematic = ({ items }: MdxSchematicProps) => {
   return (
-    <div className="p-10 bg-blue-100/30 rounded my-4 flex justify-between items-center w-fit gap-4 mx-auto border border-gray-300">
-      <MdxSchematic.item index={1} title="텍스트" to="음성" />
-      <MdxSchematic.item index={2} title="음성" />
+    <div className="p-10 bg-blue-100/30 rounded my-4 flex justify-between items-center w-fit gap-44 mx-auto border border-gray-300">
+      {items.map(item => {
+        return <MdxSchematic.item {...item} key={item.index} />;
+      })}
     </div>
   );
 };
@@ -34,7 +36,7 @@ MdxSchematic.item = ({ index, title, to, way = "one" }: IItemProps) => {
       <div className="p-8 rounded" style={{ backgroundColor: color }} id={title}>
         <BaseText className="font-bold">{title}</BaseText>
       </div>
-      {to && <MdxSchematic.arrow from={title} to={to} way={way} />}
+      {to && <MdxSchematic.arrow from={title} to={to} way={way}></MdxSchematic.arrow>}
     </>
   );
 };
@@ -44,29 +46,65 @@ interface IArrowProps {
   to: string;
   way: "one" | "two";
 }
+interface ICoordinates {
+  x: number;
+  y: number;
+}
 
 const getCenterPoint = ({
   offsetLeft,
   offsetWidth,
   offsetTop,
   offsetHeight,
-}: HTMLElement): { x: number; y: number } => {
+}: HTMLElement): ICoordinates => {
   return { x: offsetLeft + offsetWidth / 2, y: offsetTop + offsetHeight / 2 };
 };
 
+const getDegByCoords = ({ x: fromX, y: fromY }: ICoordinates, { x: toX, y: toY }: ICoordinates) => {
+  const angleRad = Math.atan2(toY - fromY, toX - fromX);
+  return angleRad * (180 / Math.PI);
+};
+
+const getMedian = (from: number, to: number) => {
+  if (to - from === 0) {
+    return "auto";
+  }
+
+  return (to - from) / 2 + from - 35;
+};
+
+const getArrowCoords = (
+  { x: fromX, y: fromY }: ICoordinates,
+  { x: toX, y: toY }: ICoordinates,
+): CSSProperties => {
+  return { left: getMedian(fromX, toX), top: getMedian(fromY, toY) };
+};
+
 MdxSchematic.arrow = ({ from, to, way }: IArrowProps) => {
-  const [deg, setDeg] = useState(0);
+  const [fromCoords, setFromCoords] = useState<ICoordinates>();
+  const [toCoords, setToCoords] = useState<ICoordinates>();
 
   useEffect(() => {
     const fromEl = document.getElementById(from);
     const toEl = document.getElementById(to);
     if (fromEl && toEl) {
-      const { x: fromX, y: fromY } = getCenterPoint(fromEl);
-      const { x: toX, y: toY } = getCenterPoint(toEl);
-      const angleRad = Math.atan2(toY - fromY, toX - fromX);
-      const angleDeg = angleRad * (180 / Math.PI);
-      setDeg(angleDeg);
+      setFromCoords(getCenterPoint(fromEl));
+      setToCoords(getCenterPoint(toEl));
     }
   }, []);
-  return <Icon kind="arrow_right_alt" size={70} style={{ transform: `rotate(${deg}deg)` }} />;
+
+  if (!fromCoords || !toCoords) {
+    return <></>;
+  }
+  return (
+    <Icon
+      kind="arrow_right_alt"
+      className="absolute"
+      size={70}
+      style={{
+        transform: `rotate(${getDegByCoords(fromCoords, toCoords)}deg)`,
+        ...getArrowCoords(fromCoords, toCoords),
+      }}
+    />
+  );
 };

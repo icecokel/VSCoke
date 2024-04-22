a64a8186d24041f7a592ed9d953effd726ff2d6a
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-04-22 20:57:08 +0900

    BaseText 프롭스 수정,

--- src/components/baseUi/Text/index.tsx
+++ src/components/baseUi/Text/index.tsx
@@ -6,11 +6,16 @@ import { HTMLAttributes, createElement } from "react";
 
 interface IBaseTextProps extends HTMLAttributes<HTMLSpanElement> {
   type?: TTextType;
+  fontWeight?: number;
 }
 
-const BaseText = ({ children, type = "body1", ...props }: IBaseTextProps) => {
+const BaseText = ({ children, type = "body1", fontWeight, style, ...props }: IBaseTextProps) => {
   if (!["body1", "body2", "caption"].includes(type)) {
-    return createElement(type, { className: styles[type], ...props }, children);
+    return createElement(
+      type,
+      { className: styles[type], style: { ...style, fontWeight: fontWeight }, ...props },
+      children,
+    );
   }
   return createElement("span", { className: styles[type], ...props }, children);
 };

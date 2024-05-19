1395c2a176ff0410d4091b6cbf7ec426b54971d0
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-05-19 18:25:58 +0900

    패키지 변경

---
+++ src/components/baseUi/SlideGroup/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, { HTMLAttributes, ReactNode } from 'react';
+import Slide, { ISlideProps } from '../Slide';
+
+interface ISlideGroupProps extends Omit<ISlideProps, 'delay'> {
+  children: ReactNode;
+  delay?: number;
+  className?: HTMLAttributes<HTMLDivElement>['className'];
+}
+
+const SlideGroup = ({ children, delay = 250, direction, className }: ISlideGroupProps) => {
+  return (
+    <div className={className}>
+      {React.Children.map(children, (child, index) => {
+        return (
+          <Slide active delay={delay * index + 1} fillMode="backwards" direction={direction}>
+            {child}
+          </Slide>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SlideGroup;

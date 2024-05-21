e6b8cb6ad3cf679d002a9b7b4d897e7df4c1297c
DESKTOP-O3I6NT4\LeeSangmin <red9runge@gmail.com>
2024-05-21 23:26:07 +0900

    다이얼로그 패키지 구조 변경

--- src/components/baseUi/Dialog/index.tsx
+++ src/components/baseUi/Dialog/index.tsx
@@ -1,6 +1,7 @@
-import Icon from "./Icon";
-import Modal, { IModalProps } from "./Modal";
-import BaseText from "./Text";
+import Icon from "../Icon";
+import Modal, { IModalProps } from "../Modal";
+import BaseText from "../Text";
+import styles from "./style.module.css";
 
 interface IDialogProps extends IModalProps {
   open: boolean;
@@ -11,9 +12,9 @@ interface IDialogProps extends IModalProps {
 const Dialog = ({ open, title, children, onClose }: IDialogProps) => {
   return (
     <Modal open={open} onClose={onClose}>
-      <div className="flex justify-between items-center mb-3">
+      <div className={styles.wrapper}>
         {title && <BaseText type="h6">{title}</BaseText>}
-        <Icon kind="close" className="text-gray-500" />
+        <Icon kind="close" className={styles.icon} />
       </div>
       <div>{children}</div>
     </Modal>

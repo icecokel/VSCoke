0c8e14975196ac43ab51aa6672db9c4af3fecf7c
icecokel <red9runge@gmail.com>
2024-06-01 17:45:00 +0900

    모달 공통 바로가기 종료 옵션 추가

--- src/components/baseUi/Modal/index.tsx
+++ src/components/baseUi/Modal/index.tsx
@@ -1,15 +1,19 @@
 import styles from "./style.module.css";
 import useClickOutSide from "@/hooks/useClickOutSide";
+import useShortCut from "@/hooks/useShortCut";
 import { IHaveChildren } from "@/models/common";
 import { createPortal } from "react-dom";
 
 export interface IModalProps extends IHaveChildren {
   open: boolean;
   onClose: () => void;
+  disabledEscape?: boolean;
 }
 
-const Modal = ({ onClose, open, children }: IModalProps) => {
+const Modal = ({ onClose, open, children, disabledEscape = false }: IModalProps) => {
   const ref = useClickOutSide(onClose);
+  const key = disabledEscape ? [] : ["escape"];
+  useShortCut(key, onClose);
   return (
     <>
       {open &&

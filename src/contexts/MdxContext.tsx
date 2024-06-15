927639f0b5513fb92ebd129fa34f603a8bfc1965
icecokel <red9runge@gmail.com>
2024-06-15 20:44:28 +0900

    블로그 제목 겹쳐지지 않도록 수정

--- src/contexts/MdxContext.tsx
+++ src/contexts/MdxContext.tsx
@@ -2,6 +2,7 @@
 
 import { TVariant } from "../components/mdx/MdxLinkHead";
 import { IHaveChildren } from "@/models/common";
+import { usePathname } from "next/navigation";
 import { createContext, useEffect, useState } from "react";
 
 interface INav {
@@ -11,20 +12,24 @@ interface INav {
 
 interface ILinkNav {
   label: string;
+  top: number;
   type: TVariant;
 }
 
 interface IMdxContext {
   nav: INav[];
   add: (nav: INav) => void;
+  stickyHead?: ILinkNav;
 }
 
 export const mdxContext = createContext<IMdxContext>({} as IMdxContext);
 
 const MdxProvider = ({ children }: IHaveChildren) => {
-  const [nav, setNav] = useState<INav[]>([]);
+  const [navList, setNavList] = useState<INav[]>([]);
+  const [stickyHead, setStickyHead] = useState<ILinkNav>();
+  const pathname = usePathname();
   const add = (target: INav) => {
-    setNav(prev => {
+    setNavList(prev => {
       const navToUpdate = prev.find(({ title }) => title === target.title);
       if (navToUpdate) {
         return [...prev].map(item => {
@@ -39,7 +44,25 @@ const MdxProvider = ({ children }: IHaveChildren) => {
     });
   };
 
-  return <mdxContext.Provider value={{ nav, add }}>{children}</mdxContext.Provider>;
+  const handleScroll = () => {
+    const currentLinks = navList.find(({ title }) => title === pathname)?.items ?? [];
+    const sortedLinks = [...currentLinks].reverse();
+
+    const activeLink = sortedLinks.find(item => item.top < document.documentElement.scrollTop);
+
+    setStickyHead(prev => (prev?.top !== activeLink?.top ? activeLink : prev));
+  };
+
+  useEffect(() => {
+    if (navList.length > 0) {
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
+  }, [navList]);
+
+  return (
+    <mdxContext.Provider value={{ nav: navList, add, stickyHead }}>{children}</mdxContext.Provider>
+  );
 };
 
 export default MdxProvider;

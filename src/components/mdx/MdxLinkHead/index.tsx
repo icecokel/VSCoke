cbd0021362ef3585a3fcdd747c3bff7c538f3299
icecokel <red9runge@gmail.com>
2024-06-15 20:44:28 +0900

    블로그 제목 겹쳐지지 않도록 수정

--- src/components/mdx/MdxLinkHead/index.tsx
+++ src/components/mdx/MdxLinkHead/index.tsx
@@ -3,7 +3,7 @@
 import styles from "./style.module.css";
 import { mdxContext } from "@/contexts/MdxContext";
 import { usePathname } from "next/navigation";
-import { createElement, useContext, useEffect } from "react";
+import { createElement, useContext, useEffect, useRef } from "react";
 
 export type TVariant = "h1" | "h2" | "h3";
 
@@ -15,18 +15,26 @@ interface MdxLinkHeadProps {
 export const PREFIX = "link-title";
 
 const MdxLinkHead = ({ children, variant }: MdxLinkHeadProps) => {
-  const { add } = useContext(mdxContext);
+  const { add, stickyHead } = useContext(mdxContext);
   const pathname = usePathname();
+  const ref = useRef<HTMLElement>(null);
 
   const id = `${PREFIX}-${children[1]}`;
 
   useEffect(() => {
-    add({ title: pathname, items: [{ type: variant, label: children[1] }] });
+    add({
+      title: pathname,
+      items: [{ type: variant, label: children[1], top: ref.current?.offsetTop ?? 0 }],
+    });
   }, []);
+
+  const isSticky = stickyHead?.label === children[1];
 
   return createElement(variant, {
     id: `${id}`,
     className: styles[variant],
+    style: { position: isSticky ? "sticky" : "unset" },
+    ref: ref,
     children: <a href={`#${id}`}>{children}</a>,
   });
 };

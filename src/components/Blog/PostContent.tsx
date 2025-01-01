80f027f26ca6ea874907172e6ef946907c6a244f
icecokel <red9runge@gmail.com>
2025-01-01 17:15:25 +0900

    설정 수정 및 라이브러리 업그레이드

--- src/components/Blog/PostContent.tsx
+++ src/components/Blog/PostContent.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import HeadTitle from "./HeadTitle";
 import NameCard from "./NameCard";
-import {MDXRemote} from "next-mdx-remote";
+import { MDXRemote } from "next-mdx-remote";
 
 interface PostContentProps {
-  frontMatter: {
+  frontMatter?: {
     title: string;
     date: string;
   };
@@ -15,10 +15,10 @@ interface PostContentProps {
 export default function PostContent({ frontMatter, content }: PostContentProps) {
   return (
     <article className="py-8 w-full">
-      <HeadTitle title={frontMatter.title} date={frontMatter.date} />
+      {frontMatter && <HeadTitle title={frontMatter.title} date={frontMatter.date} />}
       <hr />
       <MDXRemote {...content} />
       <NameCard />
     </article>
   );
-} 
+}

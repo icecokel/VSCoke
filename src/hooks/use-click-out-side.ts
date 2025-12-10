87e05a5693e2b3934d9395788ea99a952c1ffc29
icecokel <red9runge@gmail.com>
2025-12-10 23:50:42 +0900

    fix: TypeScript 타입 에러 수정
    - useClickOutSide: any 타입 사용 (제네릭 문제 해결)
    - useShortCut: Function -> () => void 타입 변경
    - useSortByKey: Record<string, any> 타입 사용
    - debounce: any[] 타입에 eslint-disable 추가

---
+++ src/hooks/use-click-out-side.ts
@@ -0,0 +1,22 @@
+import { RefObject, useEffect, useRef } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useClickOutSide = (onClickOutSide: () => void): RefObject<any> => {
+  const ref = useRef<HTMLElement>(null);
+  const handleClickOutSide = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      onClickOutSide();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutSide);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutSide);
+    };
+  }, []);
+
+  return ref;
+};
+
+export default useClickOutSide;
---
+++ src/hooks/use-short-cut.ts
@@ -0,0 +1,19 @@
+import useKeyPress from "@/hooks/use-key-press";
+import { useEffect } from "react";
+
+/**
+ *
+ * @param keys @see https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
+ * @param cb
+ */
+const useShortCut = (keys: KeyboardEvent["key"][], cb: () => void) => {
+  const { pushedKey, isSubset } = useKeyPress();
+
+  useEffect(() => {
+    if (isSubset(keys)) {
+      cb();
+    }
+  }, [pushedKey]);
+};
+
+export default useShortCut;

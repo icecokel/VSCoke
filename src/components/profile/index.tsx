9db47ce45f6c2c24d7f6ee68a62ccb461da149e4
icecokel <red9runge@gmail.com>
2025-12-10 23:50:52 +0900

    feat: Profile 및 Resume 페이지 추가
    - /profile 메인 페이지 추가
    - /profile/resume 경력 페이지 추가
    - Profile 컴포넌트 작성 (소개, 링크, 스킬, 학력)
    - Resume 컴포넌트 작성 (경력 상세)
    - GitHub 아이콘 SVG 추가

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { TParentNode } from "@/models/common";
+import Avatar from "@ui/avatar";
+import Button from "@ui/button";
+import Chip from "@ui/chip";
+import Icon from "@ui/icon";
+import BaseText from "@ui/text";
+import Tooltip from "@ui/tooltip";
+import Image from "next/image";
+import Link from "next/link";
+import Github from "public/images/icons/github.svg";
+
+/**
+ * 이력서 페이지
+ * @returns 이력서 컴포넌트
+ */
+const Profile = () => {
+  return (
+    <div className="p-3 flex flex-col gap-1 md:gap-5">
+      <div className="flex items-center flex-col gap-1 md:flex-row md:gap-3  md:items-start">
+        <Avatar
+          className="h-[180px] w-[180px] border-4 border-yellow-200 md:h-[200px] md:w-[200px]"
+          src={"profileImg.jpg"}
+          size={180}
+        />
+        <div className="ml-3 mt-[25px] md:mt-0">
+          <BaseText type="h5" className="text-center">
+            코딩만 하지 않는 <br className="md:hidden" />
+            개발자 이상민입니다.
+          </BaseText>
+          <div className="mt-5 w-full max-w-sm bg-blue-100/20 rounded p-4 gap-4 flex flex-col">
+            <Tooltip text="이메일 보내기">
+              <BaseText
+                type="body1"
+                className="flex items-center gap-x-1 hover:text-yellow-200 font-bold"
+              >
+                <Icon kind="mail" />
+                <a href="mailto:red9runge@gmail.com">red9runge@gmail.com</a>
+              </BaseText>
+            </Tooltip>
+            <Tooltip text="전화하기">
+              <BaseText type="body1" className="flex items-center hover:text-yellow-200 font-bold">
+                <Icon kind="call" />
+                <a href="tel:01020809652">전화 걸기</a>
+              </BaseText>
+            </Tooltip>
+          </div>
+        </div>
+      </div>
+      <Profile.item title="간단 소개글">
+        <BaseText>
+          안녕하세요.
+          <br />
+          <br /> 정보통신학과를 졸업하고 인프라 엔지니어로 3년 정도 근무하다가, 반복되는 일을
+          자동화하고, 비효율적인 프로세스를 개선하는 것을 중요하게 생각했고, 저의 이런 성격이
+          개발자에 적합하다고 판단하여 개발자로 전향하게 되었습니다. <br />
+          <br />
+          일본에 있는 AllofThem이라는 회사에서 재직 중이며, FE로 근무 중이며, 필요에 따라 BE도
+          진행할 때도 있습니다. <br />
+          <br />
+          주로 신규 프로젝트 시 구축 설계 등을 진행하고 있으며, 기존 프로젝트를 진행하면 성능 개선
+          및 리팩토링을 주로 하고 있습니다. 경력
+        </BaseText>
+      </Profile.item>
+      <Profile.item title="링크">
+        <div className="flex gap-2 mb-4 w-fit hover:text-yellow-200">
+          <Image src={Github} width={24} height={24} alt="git-hub" />
+          <Link href={"https://github.com/icecokel"} target="_blank">
+            https://github.com/icecokel
+          </Link>
+        </div>
+        <div className="flex gap-2 mb-4 w-fit hover:text-yellow-200">
+          <Icon kind="bookmark" />
+          <Link href={"https://icecokel.tistory.com"} target="_blank">
+            https://icecokel.tistory.com
+          </Link>
+        </div>
+      </Profile.item>
+      <Profile.item title="스킬">
+        <div className="flex flex-wrap gap-2">
+          {SKILLS.map((item, index) => (
+            <Chip
+              key={`skill_${index}`}
+              label={item}
+              className="select-none text-white hover:border-yellow-200 hover:text-yellow-200"
+            />
+          ))}
+        </div>
+      </Profile.item>
+
+      <Profile.item title="학력 / 교육">
+        <div className="flex flex-col sm:flex-row gap-2 sm:items-center ">
+          <div className="mb-5 min-w-[140px]">2019.08 - 2020.03</div>
+          <div className="mb-5">
+            <BaseText type="h6">하이브리드 앱개발(ISO&안드로이드) 및 웹 개발자 양성 과정</BaseText>
+            <BaseText type="body2">
+              Java, Spring 기반의 웹 개발 기초 (스프링 기초, Restapi) <br />
+              안드로이드, IOS 앱개발 기초
+            </BaseText>
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-2 sm:items-center ">
+          <div className="mb-5 min-w-[140px]">2010.03 - 2017.02</div>
+          <div className="mb-5">
+            <BaseText type="h6">서일대학교</BaseText>
+            <BaseText type="body2">정보통신학과</BaseText>
+          </div>
+        </div>
+      </Profile.item>
+      <Link href={"/profile/resume"}>
+        <div className="flex justify-end">
+          <Button
+            type="contained"
+            className="bg-yellow-200 !text-black hover:!text-blue-300 !rounded-[24px] flex items-center gap-x-2 shadow-[2px_4px_4px_rgb(0,0,0,0.4)]"
+          >
+            <BaseText type="body2">경력 보러가기</BaseText>
+            <Icon kind="navigation" className="rotate-[90deg]" />
+          </Button>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const ProfileItem = ({ title, children }: IItemProps) => {
+  return (
+    <div className="mt-10">
+      <BaseText type="h5">{title}</BaseText>
+      <hr className="my-5 border-white" />
+      <div>{children}</div>
+    </div>
+  );
+};
+
+Profile.item = ProfileItem;
+
+export default Profile;
+
+interface IItemProps extends TParentNode {
+  title: string;
+}
+
+const SKILLS = [
+  "React",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "HTML5",
+  "Java",
+  "Spring Boot",
+  "Spring Framework",
+  "AWS",
+  "Git",
+  "GitLab",
+  "MySQL",
+  "Figma",
+  "GraphQL",
+  "Github",
+];

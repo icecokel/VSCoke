231ccb50ef2d8f26b37af4f4b604c1d1c7961a03
icecokel <red9runge@gmail.com>
2025-12-11 10:06:37 +0900

    feat: Next.js 설정 추가
    - 이미지 원격 패턴 설정 (S3, Notion)
    - reactStrictMode false 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "icecokel-blog-dev.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/secure.notion-static.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

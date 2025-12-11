7ff3dc6a88eed185f078cb9727ec24d9f135be01
icecokel <red9runge@gmail.com>
2025-12-11 09:06:43 +0900

    feat: Context 및 모델 타입 정의 추가
    - AppProvider, HistoryContext 추가
    - 공통 타입 모델 정의 (common.ts)
    - Enum 타입 정의 (sidebar.ts)

---
+++ src/contexts/AppProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ITree } from "@/hooks/use-explorer";
+import { TParentNode } from "@/models/common";
+import SnackBarProvider from "@ui/snack-bar/context/snack-bar-provider";
+import { createContext } from "react";
+
+export const ExplorerContext = createContext<ITree[]>([]);
+
+interface IAppProviderProps extends TParentNode {
+  explorer: ITree[];
+}
+
+const AppProvider = ({ children, explorer }: IAppProviderProps) => {
+  return (
+    <SnackBarProvider>
+      <ExplorerContext.Provider value={explorer}>{children}</ExplorerContext.Provider>
+    </SnackBarProvider>
+  );
+};
+
+export default AppProvider;

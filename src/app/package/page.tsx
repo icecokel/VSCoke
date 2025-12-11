53588155c85997f3726ba3d8ebb90059ddaee8b4
icecokel <red9runge@gmail.com>
2025-12-11 09:16:52 +0900

    feat: Package 및 Readme 페이지 추가
    - /package 페이지 추가 (package.json 내용 표시)
    - /readme 페이지 추가 (환영 메시지)
    - Explorer 네비게이션 항목 연결

---
+++ src/app/package/page.tsx
@@ -0,0 +1,48 @@
+import BaseText from "@ui/text";
+import { NextPage } from "next";
+import data from "package.json";
+
+const PackagePage: NextPage = () => {
+  const renderValue = (value: any) => {
+    if (typeof value === "object") {
+      if (Array.isArray(value)) {
+        return (
+          <ul>
+            &#123;
+            {value.map((item: any, index: number) => (
+              <li key={index} className="ml-[1em]">
+                <BaseText>{renderValue(item)}</BaseText>
+              </li>
+            ))}
+            &#125;
+          </ul>
+        );
+      } else {
+        return (
+          <ul className="ml-[1em]">
+            &#123;
+            {Object.keys(value).map((key: string, index: number) => (
+              <li key={index} className="ml-[1em]">
+                <BaseText className="font-bold mr-1">&quot;{key}&quot;</BaseText>:
+                <BaseText className="ml-1">{renderValue(value[key])}</BaseText>
+              </li>
+            ))}
+            &#125;
+          </ul>
+        );
+      }
+    } else {
+      return typeof value === "string" ? `"${value}"` : value;
+    }
+  };
+
+  return (
+    <div className="p-5 bg-gray-700 rounded">
+      <pre className="-ml-[1em]">
+        <code>{renderValue(data)}</code>
+      </pre>
+    </div>
+  );
+};
+
+export default PackagePage;

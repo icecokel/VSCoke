85364c76ad94ef2568d9b0a70599ca7dc5fc8a7c
icecokel <red9runge@gmail.com>
2025-12-11 09:06:49 +0900

    feat: 커스텀 React Hooks 추가
    - useBoolean: boolean 상태 관리 훅
    - useExplorer: 파일 탐색기 데이터 관리 훅
    - useHistory: 히스토리 관리 훅
    - useKeyPress: 키보드 입력 감지 훅

---
+++ src/hooks/use-explorer.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import useSortByKey from "./use-sort-by-key";
+import { ExplorerContext } from "@/contexts/AppProvider";
+import { useContext, useEffect, useState } from "react";
+
+export interface ITree {
+  index?: number;
+  id: string;
+  label: string;
+  icon?: string;
+  path?: string;
+  items?: ITree[];
+}
+
+const useExplorer = () => {
+  const [itemList, setItemList] = useState<ITree[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const explorer = useContext(ExplorerContext);
+
+  useEffect(() => {
+    if (explorer) {
+      setItemList([...explorer]);
+      setIsLoading(false);
+    }
+  }, [explorer]);
+
+  const result = useSortByKey({ items: itemList, key: "index" });
+
+  return {
+    isLoading,
+    itemList: result,
+  };
+};
+
+export default useExplorer;
